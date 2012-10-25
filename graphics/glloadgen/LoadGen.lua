b00fda8a74b5bd7bf2084573735b389982b14639
alfonse <devnull@localhost>
2012-10-25 12:38:51 -0700

    No more Generate.lua. It's all structures now baby!

--- graphics/glloadgen/LoadGen.lua
+++ graphics/glloadgen/LoadGen.lua
@@ -15,8 +15,12 @@ end
 FixupPath = SysRelPath --Older name.
 
 local opts = require "GetOptions"
-local gen = require "Generate"
+local Specs = require "Specs"
+local Styles = require "Styles"
+local LoadSpec = require "LoadLuaSpec"
+local util = require "util"
 
+--Get the options.
 local status, options = pcall(opts.GetOptions, arg)
 
 if(not status) then
@@ -24,6 +28,38 @@ if(not status) then
 	return
 end
 
-gen.Generate(options)
+--Load the spec data.
+local spec = Specs.GetSpec(options.spec)
+local specData = spec.LoadSpec()
+
+--Verify that every extension in `options.extensions` is a real extension.
+local badExts = {}
+for _, extName in ipairs(options.extensions) do
+	if(not specData.extdefs[extName]) then
+		badExts[#badExts + 1] = extName
+	end
+end
+
+if(#badExts > 0) then
+	io.stdout:write("The following extensions are not in the spec ", options.spec, ":\n")
+	for _, extName in ipairs(badExts) do
+		io.stdout:write("\t", extName, "\n")
+	end
+	return
+end
+
+--Extract the path and base-filename from the filename.
+local simplename, dir = util.ParsePath(options.outname)
+dir = dir or "./"
+
+assert(simplename,
+	"There is no filename in the path '" .. options.outname .. "'")
+
+local style, structure = Styles.GetStyle(options.style)
+
+--Compute the filename, minus style-specific suffix.
+local basename = dir .. spec:FilePrefix() .. simplename
+
+structure.Proc(basename, style, specData, spec, options)
 
 
