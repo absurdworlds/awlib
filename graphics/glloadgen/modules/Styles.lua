e6db72a5b3335add830fd578e95605051aa01cbe
alfonse <devnull@localhost>
2012-12-01 22:25:50 -0800

    glload style transfered to the SDK, where it belongs.

--- graphics/glloadgen/modules/Styles.lua
+++ graphics/glloadgen/modules/Styles.lua
@@ -13,7 +13,6 @@ local style_registry =
 {
 	pointer_c = require("StylePointerC"),
 	pointer_cpp = require("StylePointerCPP"),
-	glload = require("StyleGLLoad"),
 	noload_cpp = require("StyleNoloadCpp"),
 }
 
