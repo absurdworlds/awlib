430c62c599037a4cfe0a40745cc605693437c99f
alfonse <devnull@localhost>
2013-06-21 16:41:04 -0700

    Lua file to build conversion.

---
+++ graphics/glloadgen/ConvertToLua.lua
@@ -0,0 +1,22 @@
+local LoadXml = require "LoadXml"
+require "_TableWriter"
+local Build = require "_BuildDataFromXml"
+
+local specs =
+{
+	{file="gl.xml", out="glspec.lua", api="gl",  support="gl",  enumPrefix="GL_",  funcPrefix="gl"},
+	{file="wgl.xml", out="wglspec.lua", api=nil, support="wgl", enumPrefix="WGL_", funcPrefix="wgl"},
+	{file="glx.xml", out="glxspec.lua", api=nil, support="glx", enumPrefix="GLX_", funcPrefix="glX"},
+}
+
+for _, spec in ipairs(specs) do
+	local xmlData, errors = LoadXml.LoadXml("newglspecs/" .. spec.file)
+
+	local data = Build.BuildData(xmlData, spec.api,
+		spec.support, spec.enumPrefix, spec.funcPrefix)
+
+	local hFile = io.open("newglspecs/" .. spec.out, "w")
+	WriteTable(hFile, data)
+	hFile:close()
+end
+
