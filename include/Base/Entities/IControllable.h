19c3acf5dd288a712856b0b58a028e5d465fd09d
haddayn <hededrk@gmail.com>
2014-05-15 08:56:01 +0400

    darn that git

---
+++ include/Base/Entities/IControllable.h
@@ -0,0 +1,20 @@
+
+#ifndef __H_INCLUDED__HRENGIN_IControllable
+#define __H_INCLUDED__HRENGIN_IControllable
+
+#include "GUI/Input/hrInputEvent.h"
+
+namespace hrengin
+{
+
+class IControllable
+{
+public:
+	virtual bool ReceiveInput(gui::hrInputEvent input) = 0;
+	virtual bool IsEnabled() = 0;
+};
+
+} // namespace hrengin
+
+
+#endif//__H_INCLUDED__HRENGIN_IControllable
