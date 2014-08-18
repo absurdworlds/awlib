c55622d063eb6ab9971d7e4f3b28c9a3da06123f
Hudd <hededrk@mail.com>
2014-08-19 00:18:56 +0400

    Filesystem: Create implementation for IBufferedStream
    Added class CBufferedStream

---
+++ source/hrenginfilesys/CBufferedStream.cpp
@@ -0,0 +1,52 @@
+
+#include <hrengin/filesystem/IReadFile.h>
+
+#include "CBufferedStream.h"
+
+namespace hrengin {
+namespace io {
+
+const u32 STREAM_BUFFER_SIZE = 1024;
+
+
+IBufferedStream* createBufferedStream(IReadFile* source)
+{
+	return new CBufferedStream(source);
+}
+
+CBufferedStream::CBufferedStream(IReadFile* source)
+: source_(source)
+{
+	source_->read(buffer_, STREAM_BUFFER_SIZE);
+}
+
+CBufferedStream::~CBufferedStream()
+{
+	delete[] buffer_;
+}
+
+bool CBufferedStream::getCurrent(char& c)
+{
+	return buffer_[pos_ % STREAM_BUFFER_SIZE];
+}
+
+bool CBufferedStream::getNext(char& c)
+{
+	char c = buffer_[pos_ % STREAM_BUFFER_SIZE];
+
+	pos_++;
+	
+	if(pos_ > source_->getSize()) {
+		return 0;
+	}
+
+	if(pos_ % STREAM_BUFFER_SIZE == 0) {
+		source_->read(buffer_, STREAM_BUFFER_SIZE);
+	}
+
+	return c;
+}
+
+
+} //namespace io
+} //namespace hrg
