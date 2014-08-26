6dd15b80b51d95d9a6a4dd853e400c250fcdd5d4
Hudd <hededrk@gmail.com>
2014-08-26 22:01:07 +0400

    Filesystem: Add datapath to hrenginpaths

--- include/hrengin/filesystem/hrenginpaths.h
+++ include/hrengin/filesystem/hrenginpaths.h
@@ -1,5 +1,5 @@
-#ifndef __HRENGIN_FILESYSTEM__hrenginpaths_H__
-#define __HRENGIN_FILESYSTEM__hrenginpaths_H__
+#ifndef _hrengin_hrenginpaths_
+#define _hrengin_hrenginpaths_
 
 #include <string>
 //#include <Irrlicht/path.h>
@@ -7,10 +7,11 @@
 namespace hrengin {
 namespace io {
 
+const std::string datapath("../data/");
 const std::string modelpath("../data/models/");
 const std::string soundpath("../data/sound/");
 
 }
 }
 
-#endif  __HRENGIN_FILESYSTEM__hrenginpaths_H__
+#endif //_hrengin_hrenginpaths_
