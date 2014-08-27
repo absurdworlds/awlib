740204b94c9bfc2b6fa27666ec4dac4ddc36de02
Hudd <hededrk@gmail.com>
2014-08-28 01:32:19 +0400

    Common: Add BIT macro & Add description
    o Moved BIT(X) macro from IPhysicsManager.h
    o Added some description for those who unfamiliar with macros.

--- include/hrengin/common/hrenginmacro.h
+++ include/hrengin/common/hrenginmacro.h
@@ -1,21 +1,33 @@
-#ifndef __HRENGIN_hrenginmacro_H__
-#define __HRENGIN_hrenginmacro_H__
+#ifndef _hrengin_hrenginmacro_
+#define _hrengin_hrenginmacro_
 
-#include <hrengin/config/hrConfig.h>
+#include <hrengin/common/hrenginconfig.h>
+
+/* Make a bit mask of a desired bit */
+#define BIT(x) (1<<(x))
 
 #define __FUNC__ __func__ 
 
+/* Stringize */
 #define TO_STR(x) TO_STR1(x)
 #define TO_STR1(x) #x
 
+/* Concatenate */
 #define CONCAT(arg1, arg2)   CONCAT1(arg1, arg2)
 #define CONCAT1(arg1, arg2)  CONCAT2(arg1, arg2)
 #define CONCAT2(arg1, arg2)  arg1##arg2
 
+/* Expand tokens — stupid MSVC glues __VA_ARGS__ */
 #define MSVC_DEVELOPERS_MUST_BE_FUCKED_BY_KEYMAN_TEN_TIMES_EACH(x) x
 #define FUCK_MSVC_DEVELOPERS(x) x
 #define EXPAND(x) x
 
+/* Vararg processing macros
+   Uses the trick: depending on number of arguments, the desired macro shifts
+   to a NAME position.
+
+   Use FOR_EACH macro, other macros are to be used only internally by FOR_EACH
+ */
 #define GET_MACRO(_1,_2,_3,_4,_5,_6,NAME,...) NAME
 
 #define FE_1(DO, X, ...) EXPAND(DO(X))
@@ -31,4 +43,4 @@
 	EXPAND(GET_MACRO(__VA_ARGS__, FE_6, FE_5, FE_4, FE_3, FE_2, FE_1)(NAME, __VA_ARGS__))
 
 
-#endif//__HRENGIN_hrenginapi_H__
+#endif//_hrengin_hrenginmacro_
