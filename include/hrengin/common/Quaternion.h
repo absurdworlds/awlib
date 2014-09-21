e50f96a216b78db65616597a3854537111c81224
Hudd <hededrk@gmail.com>
2014-09-22 01:07:59 +0400

    Common: Add quaternion interpolation functions

--- include/hrengin/common/Quaternion.h
+++ include/hrengin/common/Quaternion.h
@@ -62,6 +62,14 @@ public:
 	{
 	}
 
+	void set(Quaternion<T> const& other)
+	{
+		X = other.X;
+		Y = other.Y;
+		Z = other.Z;
+		W = other.W;
+	}
+
 	Quaternion<T>& setEuler(T x, T y, T z)
 	{
 	
@@ -94,11 +102,85 @@ public:
 		return euler * math::RADTODEG64;
 	}
 
+	T dot (Quaternion<T> const& other)
+	{
+		return X*other.X + Y*other.Y + Z*other.Z + W*other.W;
+	}
+
+	Quaternion<T>& normalize()
+	{
+		T mag = X*X + Y*Y + Z*Z + W*W;
+		if(math::equals(mag, T(1.0))) {
+			return *this;
+		}
+
+		mag = math::invSqrt(mag);
+
+		X *= mag;
+		Y *= mag;
+		Z *= mag;
+		W *= mag;
+
+		return *this;
+	}
+	
 	T X;
 	T Y;
 	T Z;
 	T W;
 };
+
+template <typename T>
+Quaternion<T>& lerp (Quaternion<T> const& q0, Quaternion<T> const& q1, f64 t)
+{
+	
+	f64 const inv = 1.0 - t;
+	return Quaternion<T>(T(v0.X*inv + v1.X*t),
+		T(v0.Y*inv + v1.Y*t),
+		T(v0.Z*inv + v1.Z*t),
+		T(v0.W*inv + v1.W*t));
+}
+
+template <typename T>
+Quaternion<T>& nlerp (Quaternion<T> const& q0, Quaternion<T> const& q1, f64 t)
+{
+	return lerp(q1, q2, t).normalized();
+}
+template <typename T>
+Quaternion<T> slerp (Quaternion<T> const& q1, Quaternion<T> const& q2,
+	f64 alpha, f64 threshold = 0.995)
+{
+	T dot = dot(other);
+	
+	if(dot > T(threshold)) {
+		return lerp(q1, q2, alpha);
+	}
+
+	// use shortest path
+	if (dot < 0) {
+		q1.x = -q1.x;
+		q1.y = -q1.y; 
+		q1.z = -q1.z;
+		dot = -dot;
+	}
+
+#if 0 // alternative implementation
+	math::clamp(dot, T(-1.0), T(1.0));
+	T const theta = acos(dot) * alpha;
+
+	Quaternion<T> q3 = q2 - q1*dot;
+	q3.normalize();
+
+	set(q1*sin(theta) + q3*cos(theta))
+#else
+	T const theta = acos(dot);
+	T const rsin = 1/sqrt(1.0 - dot*dot);
+	T const t1 = sin((1.0 - alpha)*theta) * rsin;
+	T const t2 = sin(alpha*theta) * rsin;
+#endif
+	return q1*t1 + q2*t2;
+}
+
 
 } // namespace hrengin
 
