9321ddafdf558ceec425bb4108552129fc473bb0
Hudd <hededrk@gmail.com>
2014-10-09 06:18:54 +0400

    Common: Replace individual components of Vector3d with array
    With temporary backward compatibility hack employing references

--- include/hrengin/common/Vector3d.h
+++ include/hrengin/common/Vector3d.h
@@ -9,7 +9,12 @@
 #ifndef _hrengin_Vector3d_
 #define _hrengin_Vector3d_
 
+#include <hrengin/common/config.h>
 #include <hrengin/common/math.h>
+
+#ifdef HR_COMPILER_MSC // fucking microsoft
+#include <array>
+#endif
 
 namespace hrengin {
 
@@ -18,68 +23,82 @@ template <class T>
 class Vector3d {
 public:
 	//! Default constructor. Constructs zero vector.
-	Vector3d() 
-		: x(0), y(0), z(0) 
-	{
-	}
-	
-	//! Construct vector with individual coodrinates
-	Vector3d(T const x, T const y, T const z) 
-		: x(x), y(y), z(z) 
+	Vector3d ()
+		: coord_(0),
+		x(coord_[0]), y(coord_[1]), z(coord_[2])
 	{
 	}
 
 	//! Construct vector with same value for coordinates
-	explicit Vector3d(T const v) 
-		: x(v), y(v), z(v) 
+	explicit Vector3d (T const v) 
+		: coord_(v),
+		x(coord_[0]), y(coord_[1]), z(coord_[2])
+	{
+	}
+	
+	//! Construct vector with individual coodrinates
+	Vector3d (T const x, T const y, T const z)
+#ifndef HR_COMPILER_MSC // fucking microsoft
+		: coord_{{x, y, z}},
+#else
+		: coord_({x, y, z}),
+#endif
+		x(coord_[0]), y(coord_[1]), z(coord_[2])
 	{
 	}
 
 	//! Copy constructor
-	Vector3d(Vector3d<T> const& other) 
-		: x(other.x), y(other.y), z(other.z) 
+	Vector3d (Vector3d<T> const& other) 
+#ifndef HR_COMPILER_MSC // fucking microsoft
+		: coord_{{other[0], other[1], other[2]}},
+#else
+		: coord_({other[0], other[1], other[2]}),
+#endif
+		x(coord_[0]), y(coord_[1]), z(coord_[2])
 	{
 	}
 	
 	//! Predefined instance of zero vector
 	static Vector3d<T> const zero;
 
+	//! Set coordinates of the vector
 	void set (T const x, T const y, T const z)
 	{
-		this->x = x; 
-		this->y = y; 
-		this->z = z; 
+		coord_[0] = x;
+		coord_[1] = y;
+		coord_[2] = z;
 	}
-
+	
+	//! Copy coordinates of other vector
 	void set (Vector3d<T> const& other)
 	{
-		x = other.x; 
-		y = other.y; 
-		z = other.z; 
+		coord_[0] = other[0];
+		coord_[1] = other[1];
+		coord_[2] = other[2];
 	}
-
 	
+	//! Copy coordinates of other vector
 	Vector3d<T>& operator = (Vector3d<T> const& other)
 	{
-		x = other.x; 
-		y = other.y; 
-		z = other.z; 
+		set(other);
 		return *this;
 	}
 	
+	//! Vector addition
 	Vector3d<T> operator + (Vector3d<T> const& other) const
 	{
-		return Vector3d<T>(x + other.x, y + other.y, z + other.z); 
+		return Vector3d<T>(x + other[0], y + other[1], z + other[2]); 
 	}
 
+	//! Vector addition
 	Vector3d<T>& operator += (Vector3d<T> const& other)
 	{ 
-		x += other.x; 
-		y += other.y; 
-		z += other.z; 
+		coord_[0] += other[0]; 
+		coord_[1] += other[1]; 
+		coord_[2] += other[2]; 
 		return *this;
 	}
-
+	
 	Vector3d<T> operator + (T const val) const
 	{
 		return Vector3d<T>(x + val, y + val, z + val);
@@ -87,47 +106,53 @@ public:
 
 	Vector3d<T>& operator += (T const val)
 	{
-		x += val;
-		y += val;
-		z += val;
+		coord_[0] += val;
+		coord_[1] += val;
+		coord_[2] += val;
 		return *this;
 	}
 	Vector3d<T> operator - (Vector3d<T> const& other) const
 	{
-		return Vector3d<T>(x - other.x, y - other.y, z - other.z);
+		return Vector3d<T>(x - other[0], y - other[1], z - other[2]);
 	}
 
 	Vector3d<T>& operator -= (Vector3d<T> const& other)
 	{
-		x -= other.x;
-		y -= other.y;
-		z -= other.z;
+		coord_[0] -= other.[0];
+		coord_[1] -= other.[1];
+		coord_[2] -= other.[2];
 		return *this;
 	}
 
 	Vector3d<T> operator - (T const val) const
 	{
-		return Vector3d<T>(x - val, y - val, z - val);
+		return Vector3d<T>(
+			coord_[0] - val,
+			coord_[1] - val,
+			coord_[2] - val);
 	}
 
 	Vector3d<T>& operator -= (T const val)
 	{
-		x -= val;
-		y -= val;
-		z -= val;
+		coord_[0] -= val;
+		coord_[1] -= val;
+		coord_[2] -= val;
 		return *this;
 	}
 
 	Vector3d<T> operator * (Vector3d<T> const& other) const
 	{
-		return Vector3d<T>(x * other.x, y * other.y, z * other.z);
+		return Vector3d<T>(
+			coord_[0] * other[0],
+			coord_[1] * other[1],
+			coord_[2] * other[2]);
 	}
 
 	Vector3d<T>& operator *= (Vector3d<T> const& other)
 	{
-		x *= other.x;
-		y *= other.y;
-		z *= other.z;
+		coord_[0] *= other[0];
+		coord_[1] *= other[1];
+		coord_[2] *= other[2];
 		return *this;
 	}
 
@@ -138,49 +163,55 @@ public:
 
 	Vector3d<T>& operator *= (T const v)
 	{
-		x *= v;
-		y *= v;
-		z *= v;
+		coord_[0] *= v;
+		coord_[1] *= v;
+		coord_[2] *= v;
 		return *this;
 	}
 
 	Vector3d<T> operator / (Vector3d<T> const& other) const
 	{
-		return Vector3d<T>(x / other.x, y / other.y, z / other.z);
+		return Vector3d<T>(x / other[0], y / other[1], z / other[2]);
 	}
 
 	Vector3d<T>& operator /= (Vector3d<T> const& other)
 	{
-		x /= other.x;
-		y /= other.y;
-		z /= other.z;
+		coord_[0] /= other[0];
+		coord_[1] /= other[1];
+		coord_[2] /= other[2];
 		return *this;
 	}
 
 	Vector3d<T> operator / (T const val) const
 	{
 		T inv = T(1.0/val);
-		return Vector3d<T>(x * inv, y * inv, z * inv);
+		return Vector3d<T>(
+			coord_[0] * inv,
+			coord_[1] * inv,
+			coord_[2] * inv);
 	}
 
 	Vector3d<T>& operator /= (T const val)
 	{
 		T const inv = T(1.0/val);
-		x *= inv; 
-		y *= inv; 
-		z *= inv; 
+		coord_[0] *= inv; 
+		coord_[1] *= inv; 
+		coord_[2] *= inv; 
 		return *this; 
 	}
 	
 
 	Vector3d<T> operator - () const
 	{
-		return Vector3d<T>(-x, -y, -z);
+		return Vector3d<T>(
+			-coord_[0],
+			-coord_[1],
+			-coord_[2]);
 	}
 
 	
 	//! Normalize the vector
-	Vector3d<T>& normalize()
+	Vector3d<T>& normalize ()
 	{
 		f64 length = squareLength();
 		if (length == 0) {
@@ -189,15 +220,15 @@ public:
 
 		length = math::invSqrt(length);
 
-		x = T(x * length);
-		y = T(y * length);
-		z = T(z * length);
+		coord_[0] = T(coord_[0] * length);
+		coord_[1] = T(coord_[1] * length);
+		coord_[2] = T(coord_[2] * length);
 
 		return *this;
 	}
 
 	//! Get a normalized version of a vector without modifying it
-	Vector3d<T> normalized() const
+	Vector3d<T> normalized () const
 	{
 		f64 length = squareLength();
 		if (length == 0) {
@@ -206,65 +237,81 @@ public:
 
 		length = math::invSqrt(length);
 
-		return Vector3d<T>(T(x * length), T(y * length), T(z * length));
+		return Vector3d<T>(
+			T(coord_[0] * length),
+			T(coord_[1] * length),
+			T(coord_[2] * length));
 	}
 
 	//! Get length of the vector.
 	T length() const
-	{ 
-		return math::sqrt( x*x + y*y + z*z ); 
+	{
+		return math::sqrt(
+			coord_[0]*coord_[0],
+			coord_[1]*coord_[1],
+			coord_[2]*coord_[2]);
 	}
 	
 	//! Get squared length of the vector.
 	T squareLength() const
-	{ 
-		return x*x + y*y + z*z; 
+	{
+		return  coord_[0]*coord_[0] +
+			coord_[1]*coord_[1] +
+			coord_[2]*coord_[2];
 	}
 
 
 	//! Calculate the dot product with another vector
-	T dot(Vector3d<T> const& other) const
+	T dot (Vector3d<T> const& other) const
 	{
-		return x*other.x + y*other.y + z*other.z;
+		return  coord_[0]*other[0] + 
+			coord_[1]*other[1] + 
+			coord_[2]*other[2];
 	}
 
 	//! Calculate the cross product with another vector
-	Vector3d<T> cross(Vector3d<T> const& other) const
+	Vector3d<T> cross (Vector3d<T> const& other) const
 	{
-		f32 x = y * other.z - z * other.y;
-		f32 y = z * other.x - x * other.z;
-		f32 z = x * other.y - y * other.x;
+		f32 x const = coord_[1] * other[2] - coord_[2] * other[1];
+		f32 y const = coord_[2] * other[0] - coord_[0] * other[2];
+		f32 z const = coord_[0] * other[1] - coord_[1] * other[0];
 		return Vector3d<T>(x, y, z);
 	}
 
 	//! Get distance from another point
-	T getDistance(Vector3d<T> const& other) const
+	T getDistance (Vector3d<T> const& other) const
 	{
-		return Vector3d<T>(x - other.x, y - other.y, z - other.z).length();
+		return Vector3d<T>(
+			coord_[0] - other[0],
+			coord_[1] - other[1],
+			coord_[2] - other[2]).length();
 	}
 
 	//! Get squared distance from another point
-	T getDistanceSQ(Vector3d<T> const& other) const
+	T getDistanceSQ (Vector3d<T> const& other) const
 	{
-		return Vector3d<T>(x - other.x, y - other.y, z - other.z).squareLength();
+		return Vector3d<T>(
+			coord_[0] - other[0],
+			coord_[1] - other[1],
+			coord_[2] - other[2]).squareLength();
 	}
 
 	//! Set the length of the vector to a new value
-	Vector3d<T>& setLength(T const newlength)
+	Vector3d<T>& setLength (T const newlength)
 	{
 		normalize();
-		this.x *= newlength;
-		this.y *= newlength;
-		this.z *= newlength;
+		coord_[0] *= newlength;
+		coord_[1] *= newlength;
+		coord_[2] *= newlength;
 		return *this;
 	}
 	
 	// Invert the vector.
 	Vector3d<T>& invert ()
 	{
-		x *= -1;
-		y *= -1;
-		z *= -1;
+		coord_[0] *= -1;
+		coord_[1] *= -1;
+		coord_[2] *= -1;
 		return *this;
 	}
 	
@@ -280,7 +327,7 @@ public:
 	   The Z (roll) rotation is always 0, since two rotations are sufficient.
 	   (does a vector even have roll rotation? I don't think so)
 	 */
-	Vector3d<T> getHorizontalAngle() const
+	Vector3d<T> getHorizontalAngle () const
 	{
 		Vector3d<T> angle;
 
@@ -306,7 +353,7 @@ public:
 		return angle * math::RADTODEG64;
 	}
 
-	T getYaw() const
+	T getYaw () const
 	{
 		T yaw = T(atan2(f64(x), f64(z)));
 		
@@ -319,7 +366,7 @@ public:
 		return yaw * math::RADTODEG64;
 	}
 
-	T getPitch() const
+	T getPitch () const
 	{
 		f64 const xz = math::sqrt(x*x + z*z);
 		T const pitch = T(atan2(f64(xz), f64(y)) - math::HALF_PI64);
@@ -334,28 +381,84 @@ public:
 	}
 
 	//! Fill an array of 4 values with the vector data
-	void toArrayOf4(T* array) const
+	void toArrayOf4 (T* array) const
 	{
-		array[0] = x;
-		array[1] = y;
-		array[2] = z;
+		array[0] = coord_[0];
+		array[1] = coord_[1];
+		array[2] = coord_[2];
 		array[3] = 0;
 	}
 
 	//! Fill an array of 3 values with the vector data
-	void toArrayOf3(T* array) const
+	void toArrayOf3 (T* array) const
 	{
-		array[0] = x;
-		array[1] = y;
-		array[2] = z;
+		array[0] = coord_[0];
+		array[1] = coord_[1];
+		array[2] = coord_[2];
 	}
+	
+	//! Access elements of the vector by subscript
+	T& operator [] (size_t elem)
+	{
+		return coord_[elem];
+	}
+	
+	//! Access elements of the vector by subscript
+	T const& operator [] (size_t elem) const
+	{
+		return coord_[elem];
+	}
+	
+#if 0
+	//! Get coordinate along X axis
+	T& x ()
+	{
+		return coord_[0];
+	}
+	
+	//! Get coordinate along Y axis
+	T& y ()
+	{
+		return coord_[1];
+	}
+	
+	//! Get coordinate along Z axis
+	T& z ()
+	{
+		return coord_[2];
+	}
+	
+	//! Get coordinate along X axis
+	T const& x () const
+	{
+		return coord_[0];
+	}
+	
+	//! Get coordinate along Y axis
+	T const& y () const
+	{
+		return coord_[1];
+	}
+	
+	//! Get coordinate along Z axis
+	T const& z () const
+	{
+		return coord_[2];
+	}
+#endif
 
-	//! Coordinate along X axis
-	T x;
-	//! Coordinate along Y axis
-	T y;
-	//! Coordinate along Z axis
-	T z;
+	//! Temporary hack for backward compatibility
+	T& x;
+	T& y;
+	T& z;
+private:
+#ifndef HR_COMPILER_MSC // fucking microsoft
+	//! Coordinates of the vector
+	T coord_[3];
+#else
+	//! Coordinates of the vector
+	std::array<T, 3> coord_;
+#endif
 };
 
 // Initialization of static member
@@ -367,16 +470,19 @@ Vector3d<T> const Vector3d<T>::zero = Vector3d(0);
 template <>
 inline Vector3d<i32> Vector3d<i32>::operator / (i32 const val) const 
 {
-	return Vector3d<i32>(x/val, y/val, z/val);
+	return Vector3d<i32>(
+		coord_[0]/val,
+		coord_[1]/val,
+		coord_[2]/val);
 }
 
 //! Partial specialization for integer vectors
 template <>
 inline Vector3d<i32>& Vector3d<i32>::operator /= (i32 const val) 
 {
-	x /= val;
-	y /= val;
-	z /= val;
+	coord_[0] /= val;
+	coord_[1] /= val;
+	coord_[2] /= val;
 	return *this;
 }
 
