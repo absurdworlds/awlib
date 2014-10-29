15f12bb2207dc929b7a52603c38097a7ae464f44
Hudd <hededrk@gmail.com>
2014-10-29 23:12:38 +0300

    Core: Remove TRACE() from ILogger.h
    Those were used for writing debug messages, but were replaced by stubs
    some time ago (to prevent compile errors in existing code).

--- include/hrengin/core/ILogger.h
+++ include/hrengin/core/ILogger.h
@@ -17,53 +17,30 @@
 
 #include <hrengin/core/api.h>
 
-#define TRACE_FUNCTION() \
-do { \
-	hrengin::getLogger().push("DEBUG:"); \
-	hrengin::getLogger().push(__FUNC__); \
-	hrengin::getLogger().push(hrengin::endl); \
-} while (0);
-
-#define PUSHMSG(msg) hrengin::getLogger().push(msg);
-
-#define TRACE(...) \
-do { \
-	hrengin::getLogger().push("TRACE:"); \
-	hrengin::getLogger().push(__FILE__  "::" TO_STR(__LINE__) ":"); \
-	FOR_EACH(PUSHMSG,__VA_ARGS__) \
-	hrengin::getLogger().push(hrengin::endl); \
-} while (0);
-
-#undef TRACE
-#undef TRACE_FUNCTION
-
-#define TRACE(...)
-#define TRACE_FUNCTION(...)
-
 namespace hrengin {
-
 //! Interface for logger output
 class ILogBook {
 public:
-	virtual void log(std::string) = 0;
+	//! Write logger output
+	virtual void log (std::string) = 0;
 };
 
 //! Interface for logger class, which is used to write messages into log books
 class ILogger {
 public:
 	//! Virtual destructor
-	virtual ~ILogger()
+	virtual ~ILogger ()
 	{
 	}
 
-	virtual void push(std::string msg) = 0;
-	virtual void addLog(ILogBook* log) = 0;
+	virtual void push (std::string msg) = 0;
+	virtual void addLog (ILogBook* log) = 0;
 	
 	const std::string endl = std::string("\n");
 };
 
-HR_CORE_API ILogger* createLogger();
+//! Create a logger instance
+HR_CORE_API ILogger* createLogger ();
 
 } // namespace hrengin
-
 #endif//_hrengin_ILogger_
