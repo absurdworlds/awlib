0fddf6502c37109b4b64b3d0ef625840e30a1ed2
Hudd <hededrk@gmail.com>
2014-10-31 02:44:41 +0300

    Fix Matrix4_inverse.cpp test

--- tests/Matrix4_inverse.cpp
+++ tests/Matrix4_inverse.cpp
@@ -3,15 +3,15 @@
 
 void main ()
 {
-	Matrix4<double> A(
+	hrengin::Matrix4<double> A(
 		1,2,3,4,
 		5,6,7,8,
 		9,1,1,2,
 		3,4,5,1);
 		
-	Matrix4<double> B;
+	hrengin::Matrix4<double> B;
 
-	giga.getInverse(B);
+	A.getInverse(B);
 
-	Matrix4<double> I = A * B;
+	hrengin::Matrix4<double> I = A * B;
 }
