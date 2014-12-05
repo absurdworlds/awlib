7ed6c4f7890b1a40c791f863836eb33a88ee3497
Hudd <haddayn@gmail.com>
2014-12-05 14:16:48 +0000

    Reorganize `tests', add makefiles
    Moved each test case into individual folder.

---
+++ tests/Matrix4_inverse/Matrix4_inverse.cpp
@@ -0,0 +1,24 @@
+/*
+   Copyright (C) 2014  absurdworlds
+
+   License LGPLv3 or later:
+   GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
+   This is free software: you are free to change and redistribute it.
+   There is NO WARRANTY, to the extent permitted by law.
+ */
+#include <hrengin/math/Matrix4.h>
+
+void main ()
+{
+	hrengin::Matrix4<double> A(
+		1,2,3,4,
+		5,6,7,8,
+		9,1,1,2,
+		3,4,5,1);
+		
+	hrengin::Matrix4<double> B;
+
+	A.getInverse(B);
+
+	hrengin::Matrix4<double> I = A * B;
+}
