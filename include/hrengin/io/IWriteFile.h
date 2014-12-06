f91da472e42ae5aa9e8cd2dfa8997a74128a93ca
Hudd <haddayn@gmail.com>
2014-12-06 08:36:07 +0000

    io: Add IWriteFIle
    Interface for writing data into file streams.

--- include/hrengin/io/IWriteFile.h
+++ include/hrengin/io/IWriteFile.h
@@ -8,4 +8,43 @@
  */
 #ifndef _hrengin_IWriteFile_
 #define _hrengin_IWriteFile_
+
+#include <string>
+
+#include <hrengin/io/IFile.h>
+
+namespace hrengin {
+namespace io {
+//! Interface to access files for writing 
+class IWriteFile : public IFile {
+public:
+	virtual ~IWriteFile ()
+	{
+	}
+
+	/*! Write \a size bytes into file stream from \a buffer.
+	 * \param buffer Pointer to first object of source buffer.
+	 * \param size Number of bytes to read.
+	 * \return 0 if write was successful, \a -error_code otherwise.
+	 */ 
+	virtual i32 write (void const* buffer, u32 size) = 0;
+
+	/*! File access mode
+	 * \a Overwrite will destroy file contents upon opening
+	 * \a Append will keep file contents and put pointer to the end of file
+	 */
+	enum class Mode {
+		Overwrite,
+		Append
+	};
+};
+
+/*! Open file for reading
+ * \paran path Full path to file
+ * \param mode File access mode, \see IWriteFile;:Mode
+ */
+HR_IO_EXP IWriteFile* openWriteFile (std::string path, IWriteFile;:Mode mode);
+
+} // namespace io
+} // namespace hrengin
 #endif//_hrengin_IWriteFile_
