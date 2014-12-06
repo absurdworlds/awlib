50aef9329d2db7dd1150c11dbef2959aed7459e2
Hudd <haddayn@gmail.com>
2014-12-06 08:27:41 +0000

    io: Add class IFile
    This will be the base class for all types of file streams (ReadFile,
    WriteFile, CopyFile)
    It the future, probably, it will become descendant of IStream

---
+++ include/hrengin/io/IFile.h
@@ -0,0 +1,46 @@
+/*
+   Copyright (C) 2014  absurdworlds
+
+   License LGPLv3-only:
+   GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
+   This is free software: you are free to change and redistribute it.
+   There is NO WARRANTY, to the extent permitted by law.
+ */
+#ifndef _hrengin_IFile_
+#define _hrengin_IFile_
+
+#include <string>
+
+#include <hrengin/io/io.h>
+#include <hrengin/common/types.h>
+
+namespace hrengin {
+namespace io {
+//! Base class for file streams
+class IFile {
+	virtual ~IFile ()
+	{
+	}
+
+	/*! Set position indicator in file stream.
+	 * \param offset Number of bytes to move.
+	 * \param relative If true, offset will be added to current position,
+	 * otherwise to the beginning of gile.
+	 * \return 0 if seek was successful, \a -error_code otherwise
+	 */ 
+	virtual i32 seek(i32 offset, bool relative = false) = 0;
+	/* Get current position in file stream. */
+	virtual u32 tell() const = 0;
+
+	/*! Get size of file in bytes */
+	virtual u32 getSize() const = 0;
+
+	/*! \return \a true if file is open. */
+	virtual bool isOpen() const = 0;
+
+	/*! Get full path to file */
+	virtual std::string const& getPath() const = 0;
+}
+} // namespace io
+} // namespace hrengin
+#endif//_hrengin_IFile_
--- include/hrengin/io/IReadFile.h
+++ include/hrengin/io/IReadFile.h
@@ -11,15 +11,14 @@
 
 #include <string>
 
-#include <hrengin/io/io.h>
-#include <hrengin/common/types.h>
+#include <hrengin/io/IFile.h>
 
 namespace hrengin {
 namespace io {
 //! Interface to access files for reading
-class IReadFile {
+class IReadFile : public IFile {
 public:
-	virtual ~IReadFile()
+	virtual ~IReadFile ()
 	{
 	}
 
@@ -28,28 +27,10 @@ public:
 	 * \param size Number of bytes to read.
 	 * \return 0 if read was successful, \a -error_code otherwise.
 	 */ 
-	virtual i32 read(void* buffer, u32 size) = 0;
-	/*! Set position indicator in file stream.
-	 * \param offset Number of bytes to move.
-	 * \param relative If true, offset will be added to current position,
-	 * otherwise to the beginning of gile.
-	 * \return 0 if seek was successful, \a -error_code otherwise
-	 */ 
-	virtual i32 seek(i32 offset, bool relative = false) = 0;
-	/* Get current position in file stream. */
-	virtual u32 tell() const = 0;
-
-	/*! Get size of file in bytes */
-	virtual u32 getSize() const = 0;
-
-	/*! \return \a true if file is open. */
-	virtual bool isOpen() const = 0;
-
-	/*! Get full path to file */
-	virtual std::string const& getPath() const = 0;
+	virtual i32 read (void* buffer, u32 size) = 0;
 };
 
-HR_IO_EXP IReadFile* openReadFile(std::string path);
+HR_IO_EXP IReadFile* openReadFile (std::string path);
 
 } // namespace io
 } // namespace hrengin
