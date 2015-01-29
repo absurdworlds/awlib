18b61dfe5586c247895ba9e7aea0101ea90db548
Hudd <hededrk@gmail.com>
2015-01-29 22:52:29 +0000

    common: Fix typos in types.h
    Definitions for 32- and 64-bit integers were backwards.

--- include/hrengin/common/types.h
+++ include/hrengin/common/types.h
@@ -31,11 +31,11 @@ namespace hrengin {
 	typedef uint16_t		u16;
 	typedef  int16_t		i16;
 
-	typedef uint32_t		i32;
-	typedef  int32_t		u32;
+	typedef uint32_t		u32;
+	typedef  int32_t		i32;
        
-	typedef uint64_t		i64;
-	typedef  int64_t		u64;
+	typedef uint64_t		u64;
+	typedef  int64_t		i64;
 #endif
 
 typedef float				f32;
