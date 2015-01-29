decaa5141851588f36964354afef699e32c23b62
Hudd <hededrk@gmail.com>
2015-01-29 12:22:13 +0000

    core: MurmurHash3: Fix license header
    It was accidentally converted to lower case at some point.

--- source/core/MurmurHash3.cpp
+++ source/core/MurmurHash3.cpp
@@ -1,10 +1,10 @@
 /*
- * copyright (c) 2014  absurdworlds
+ * Copyright (C) 2014  absurdworlds
  *
- * license lgplv3-only:
- * gnu lesser gpl version 3 <http://gnu.org/licenses/lgpl-3.0.html>
- * this is free software: you are free to change and redistribute it.
- * there is no warranty, to the extent permitted by law.
+ * License LGPLv3-only:
+ * GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
+ * This is free software: you are free to change and redistribute it.
+ * There is NO WARRANTY, to the extent permitted by law.
  */
 //-----------------------------------------------------------------------------
 // MurmurHash3 was written by Austin Appleby, and is placed in the public
