4ab9a80a07f8f5c33976b119d7670c9602ebce1c
Hudd <hededrk@gmail.com>
2015-01-29 22:56:23 +0000

    platform: time: Improve documentation
    I've added a note about possible quirks.

--- include/hrengin/platform/time.h
+++ include/hrengin/platform/time.h
@@ -1,7 +1,7 @@
 /*
- * Copyright (C) 2014  absurdworlds
+ * Copyright (C) 2014-2015  absurdworlds
  *
- * License LGPLv3-only:
+ * License LGPLv3 or later:
  * GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
  * This is free software: you are free to change and redistribute it.
  * There is NO WARRANTY, to the extent permitted by law.
@@ -14,7 +14,11 @@
 #include <hrengin/platform/platform.h>
 
 namespace hrengin {
-//! Get system time in milliseconds
+/*!
+ * Get time elapsed from some moment.
+ * Platfrom-dependent. May not be consistent between different executions of 
+ * the application, so it should be probed at every startup.
+ */
 HR_SYS_EXP u32 getTime ();
 
 } // namespace hrengin
--- source/platform/linux/time.cpp
+++ source/platform/linux/time.cpp
@@ -1,10 +1,10 @@
 /*
-   Copyright (C) 2014  absurdworlds
-
-   License LGPLv3-only:
-   GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
-   This is free software: you are free to change and redistribute it.
-   There is NO WARRANTY, to the extent permitted by law.
+ * Copyright (C) 2014  absurdworlds
+ *
+ * License LGPLv3 or later:
+ * GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
+ * This is free software: you are free to change and redistribute it.
+ * There is NO WARRANTY, to the extent permitted by law.
  */
 #include <time.h>
 
@@ -12,7 +12,7 @@
 #include <hrengin/platform/platform.h>
 
 namespace hrengin {
-
+//! Get clock resolution, in nanoseconds
 u32 getClockResolution()
 {
 	struct timespec res;
@@ -24,6 +24,11 @@ u32 getClockResolution()
 	return 0;
 }
 
+/*!
+ * Get time elapsed from some moment.
+ * Platfrom-dependent. May not be consistent between different executions of 
+ * the application, so it should be probed at every startup.
+ */
 HR_SYS_EXP u32 getTime()
 {
 	struct timespec tp;
@@ -33,5 +38,4 @@ HR_SYS_EXP u32 getTime()
 	}
 	return 0;
 }
-
 } // namespace hrengin
