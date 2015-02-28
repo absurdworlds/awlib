5b453826945d6375c447c330a61f39080d609f5c
Hedede <hededrk@gmail.com>
2015-02-28 21:31:53 +0300

    tests: Matrix4-inverse: Add automatic test
    Compare matrix $I = A * A^{-1}$ with identity matrix. Return code
    indicates whether test failed or succeded.

--- tests/Matrix4_inverse/Matrix4_inverse.cpp
+++ tests/Matrix4_inverse/Matrix4_inverse.cpp
@@ -1,14 +1,14 @@
 /*
-   Copyright (C) 2014  absurdworlds
-
-   License LGPLv3 or later:
-   GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
-   This is free software: you are free to change and redistribute it.
-   There is NO WARRANTY, to the extent permitted by law.
+ * Copyright (C) 2014-2015  absurdworlds
+ *
+ * License LGPLv3 or later:
+ * GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
+ * This is free software: you are free to change and redistribute it.
+ * There is NO WARRANTY, to the extent permitted by law.
  */
 #include <hrengin/math/Matrix4.h>
 
-int main ()
+int main()
 {
 	hrengin::Matrix4<double> A(
 		1,2,3,4,
@@ -21,6 +21,12 @@ int main ()
 	A.getInverse(B);
 
 	hrengin::Matrix4<double> I = A * B;
+	hrengin::Matrix4<double> I2;
 
-	return 0;
+	I2.setIdentity();
+
+	if (I == I2)
+		return 0;
+
+	return 1;
 }
