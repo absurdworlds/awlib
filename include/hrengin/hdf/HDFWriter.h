f9447f1d1868b0a7c0479f6b92881323c636b712
Hedede <haddayn@gmail.com>
2015-03-31 22:38:04 +0300

    hdf: Update HDFWriter.h
    It was quite some time since HDFWriter got some love. Now I have a need
    for it (for dumping AST from hrscript). Prepare it for implementation.

--- include/hrengin/hdf/HDFWriter.h
+++ include/hrengin/hdf/HDFWriter.h
@@ -1,46 +1,37 @@
 /*
  * Copyright (C) 2014  absurdworlds
+ * Copyright (C) 2015  hedede <haddayn@gmail.com>
  *
- * License LGPLv3-only:
+ * License LGPLv3 or later:
  * GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
  * This is free software: you are free to change and redistribute it.
  * There is NO WARRANTY, to the extent permitted by law.
  */
 #ifndef _hrengin_HDFWriter_
 #define _hrengin_HDFWriter_
-
 #include <string>
 
-#include <hrengin/common/api.h>
-
-#include <hrengin/common/Vector3d.h>
-
+#include <hrengin/hdf/hdf.h>
+#include <hrengin/hdf/Value.h>
 namespace hrengin {
 namespace hdf {
+//! Interface for writing HDF files. Supports HDF 1.2.0 format.
+class Writer {
+	virtual ~Writer() {};
 
-//! Interface for writing HDF files. Can write documents in HDF 1.1.3 format.
-class HDFWriter {
-	virtual ~HDFWriter() {};
+	/*! Create a new node and write a header for it. */
+	virtual bool startNode(std::string name) = 0;
 
-	//! Write a node header, can be used in conjunction with `write*`
-	//! to produce a node-value
-	virtual bool addNodeHeader(std::string name) = 0;
-	virtual bool addNodeEnd() = 0;
+	/*! End current (bottom level) node.  */
+	virtual bool endNode() = 0;
 
-	//! Write a `value` object, must be used in conjunction with `write*`
-	virtual bool addValue(std::string name) = 0;
+	/*! Write a value object. */
+	virtual bool writeValue(std::string name, hdf::Value value) = 0;
 
-	virtual void writeFloat(f32 val) = 0;
-	virtual void writeFloat(f64 val) = 0;
-	virtual void writeInt(u32 val) = 0;
-	virtual void writeInt(i32 val) = 0;
-	virtual void writeBool(bool val) = 0;
-	virtual void writeString(std::string val) = 0;
-	virtual void writeVector3d(Vector3d<f32> val) = 0;
+	/*! Write a comment */
+	virtual void addComment(std::string comment_text) = 0;
 
-	//! Write a comment
-	virtual void addComment(std::string commentText) = 0;
-
+	/*! Report an error */
 	virtual void error(u32 type, std::string msg) = 0;
 };
 
