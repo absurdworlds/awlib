f8e2bd23e9441c94322d8fa94574cc1ab3fa93f1
Hudd <hededrk@gmail.com>
2015-09-02 19:44:10 +0300

    game: Add Unit and MapManager headers
    Currently empty.

---
+++ include/awrts/game/UnitManager.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2013-2015  absurdworlds
+ * Copyright (C)      2015  hedede <haddayn@gmail.com>
+ *
+ * License LGPLv3-only:
+ * GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
+ * This is free software: you are free to change and redistribute it.
+ * There is NO WARRANTY, to the extent permitted by law.
+ */
+#ifndef _awrts_UnitManager_
+#define _awrts_UnitManager_
+
+#include <awrts/common/types.h>
+#include <awrts/game/game.h>
+
+namespace awrts {
+namespace game {
+//! UnitManager creates and keeps track of units
+class UnitManager {
+};
+
+
+} // namespace game
+} // namespace awrts
+#endif//_awrts_UnitManager_
