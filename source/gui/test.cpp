983742883454b76ca46d962dacaafed0560c8fc9
Hudd D <hededrk@gmail.com>
2015-09-06 20:55:18 +0000

    gui: Update test.cpp

--- source/gui/test.cpp
+++ source/gui/test.cpp
@@ -3,18 +3,18 @@
 #include <awengine/gui/Canvas.h>
 #include <awengine/gui/Window.h>
 
-#include <IrrSkin.h>
+#include "IrrSkin.h"
 
 namespace awrts {
 namespace gui {
 
 int guimain()
 {
-	std::unique_ptr<Canvas> canvas = createCanvas();
-	std::unique_ptr<Skin> skin = createIrrSkin();
+	std::unique_ptr<Canvas> canvas(new Canvas());
+	std::unique_ptr<Skin> skin(new IrrSkin());
 	canvas->setSkin(skin.get());
 
-	std::unique_ptr<Window> window = createWindow(canvas.get());
+	std::unique_ptr<Window> window = std::make_unique<Window>(canvas.get());
 }
 
 }
