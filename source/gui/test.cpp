3143c1589c38bfd01bab87756be6409d9873b0d0
Hudd <hededrk@gmail.com>
2015-09-09 16:37:03 +0300

    gui: Update test

--- source/gui/test.cpp
+++ source/gui/test.cpp
@@ -2,20 +2,50 @@
 
 #include <awengine/gui/Canvas.h>
 #include <awengine/gui/Window.h>
+#include <awengine/gui/Drawer.h>
 
 #include "IrrSkin.h"
 
+#include <Irrlicht/irrlicht.h>
+#include <Irrlicht/IrrlichtDevice.h>
+#include <Irrlicht/driverChoice.h>
+using namespace irr;
 namespace awrts {
 namespace gui {
 
 int guimain()
 {
-	std::unique_ptr<Canvas> canvas(new Canvas());
-	std::unique_ptr<Skin> skin(new IrrSkin());
+	IrrlichtDevice * device = createDevice(video::EDT_OPENGL, core::dimension2d<u32>(800, 600));
+	auto driver = device->getVideoDriver();
+	std::unique_ptr<Canvas> canvas = std::make_unique<Canvas>();
+	std::unique_ptr<Skin> skin = std::make_unique<IrrSkin>(driver);
 	canvas->setSkin(skin.get());
 
-	std::unique_ptr<Window> window = std::make_unique<Window>(canvas.get());
+	auto window = std::make_unique<Window>();
+	window->setRect(Rect<f32>(0.1,0.1,0.4,0.4));
+	canvas->addElement(std::move(window));
+
+	Drawer drawer;
+
+	canvas->accept(drawer);
+
+	while(device->run() && driver)
+	if (device->isWindowActive())
+	{
+		driver->beginScene(true, true, irr::video::SColor(255,40,138,155));
+
+		canvas->accept(drawer);
+
+		driver->endScene();
+	}
+
+	device->drop();
 }
 
 }
+}
+
+int main()
+{
+	awrts::gui::guimain();
 }
