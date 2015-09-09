567b729abcc972a1b7efa8ca4207d4f0322ddb60
Hudd <hededrk@gmail.com>
2015-09-09 16:36:32 +0300

    gui: Implement IrrSkin

--- source/gui/IrrSkin.cpp
+++ source/gui/IrrSkin.cpp
@@ -6,6 +6,9 @@
  * This is free software: you are free to change and redistribute it.
  * There is NO WARRANTY, to the extent permitted by law.
  */
+#include <Irrlicht/IVideoDriver.h>
+#include <Irrlicht/IGUISkin.h>
+
 #include <awengine/gui/Canvas.h>
 #include <awengine/gui/Widget.h>
 #include <awengine/gui/Drawer.h>
@@ -13,15 +16,36 @@
 
 namespace awrts {
 namespace gui {
-
-virtual IrrSkin::~IrrSkin();
-
-void IrrSkin::drawCanvas(Rect<f32> rect)
+/*
+IrrSkin::~IrrSkin()
 {
+}*/
 
+irr::core::rect<irr::s32> toIrr(Rect<f32> awRect)
+{
+	return irr::core::rect<irr::s32>(
+	       (int)awRect.upperLeft.x(), (int)awRect.upperLeft.y(),
+	       (int)awRect.lowerRight.x(), (int)awRect.lowerRight.y());
 }
 
-void IrrSkin::drawWindowBackground(Rect<f32> w)
+void IrrSkin::drawCanvas(Rect<f32> const& rect)
+{
+}
+
+void IrrSkin::draw3DBorderInset(Rect<f32> const& w)
+{
+	//Rect screen = /*wtf*/->getScreenDimensions();
+	Vector2d<f32> screen(800,600);
+	Rect<f32> rect;
+	rect.upperLeft.x() = w.upperLeft.x() * screen.x();
+	rect.upperLeft.y() = w.upperLeft.y() * screen.y();
+	rect.lowerRight.x() = w.lowerRight.x() * screen.x();
+	rect.lowerRight.y() = w.lowerRight.y() * screen.y();
+
+	renderer.drawWindowBackground(toIrr(rect));
+}
+#if 0
+void IrrSkin::draw3DBorderOutset(Rect<f32> const& w)
 {
 	Rect screen = /*wtf*/->getScreenDimensions();
 	Rect rect;
@@ -30,55 +54,59 @@ void IrrSkin::drawWindowBackground(Rect<f32> w)
 	rect.lowerRight.x() = w.lowerRight.x() * screen.lowerRight.x();
 	rect.lowerRight.y() = w.lowerRight.y() * screen.lowerRight.y();
 
-	renderer->drawWindowBackgroundIrr(toIrr(rect));
+	renderer->drawWindowBackground(toIrr(rect));
+}
+#endif
+
+void IrrSkin::drawWindowTitleBar(Rect<f32> const& rect)
+{
 }
 
-} // namespace gui
-} // namespace awrts
 
-namespace irr {
 // for testing
 // modified code from irr::CGUISkin
-void IrrRenderer::drawWindowBackgroundIrr(irr::core::rect<s32> r)
+void IrrRenderer::drawWindowBackground(irr::core::rect<irr::s32> r)
 {
-	irr::core::rect<s32> rect = r;
+	irr::core::rect<irr::s32> rect = r;
+	std::cout << rect.LowerRightCorner.X << std::endl;
+	std::cout << rect.LowerRightCorner.Y << std::endl;
 
 	// top border
 	rect.LowerRightCorner.Y = rect.UpperLeftCorner.Y + 1;
-	Driver->draw2DRectangle(getColor(EGDC_3D_HIGH_LIGHT), rect, clip);
+	Driver->draw2DRectangle(irr::video::SColor(255,255,255,255), rect);
 
 	// left border
 	rect.LowerRightCorner.Y = r.LowerRightCorner.Y;
 	rect.LowerRightCorner.X = rect.UpperLeftCorner.X + 1;
-	Driver->draw2DRectangle(getColor(EGDC_3D_HIGH_LIGHT), rect, clip);
+	Driver->draw2DRectangle(irr::video::SColor(255,255,255,255), rect);
 
 	// right border dark outer line
 	rect.UpperLeftCorner.X = r.LowerRightCorner.X - 1;
 	rect.LowerRightCorner.X = r.LowerRightCorner.X;
 	rect.UpperLeftCorner.Y = r.UpperLeftCorner.Y;
 	rect.LowerRightCorner.Y = r.LowerRightCorner.Y;
-	Driver->draw2DRectangle(getColor(EGDC_3D_DARK_SHADOW), rect, clip);
+	Driver->draw2DRectangle(irr::video::SColor(255,50,50,50), rect);
 
 	// right border bright innner line
 	rect.UpperLeftCorner.X -= 1;
 	rect.LowerRightCorner.X -= 1;
 	rect.UpperLeftCorner.Y += 1;
 	rect.LowerRightCorner.Y -= 1;
-	Driver->draw2DRectangle(getColor(EGDC_3D_SHADOW), rect, clip);
+	Driver->draw2DRectangle(irr::video::SColor(255,130,130,130), rect);
 
 	// bottom border dark outer line
 	rect.UpperLeftCorner.X = r.UpperLeftCorner.X;
 	rect.UpperLeftCorner.Y = r.LowerRightCorner.Y - 1;
 	rect.LowerRightCorner.Y = r.LowerRightCorner.Y;
 	rect.LowerRightCorner.X = r.LowerRightCorner.X;
-	Driver->draw2DRectangle(getColor(EGDC_3D_DARK_SHADOW), rect, clip);
+	Driver->draw2DRectangle(irr::video::SColor(255,50,50,50), rect);
 
 	// bottom border bright inner line
 	rect.UpperLeftCorner.X += 1;
 	rect.LowerRightCorner.X -= 1;
 	rect.UpperLeftCorner.Y -= 1;
 	rect.LowerRightCorner.Y -= 1;
-	Driver->draw2DRectangle(getColor(EGDC_3D_SHADOW), rect, clip);
+	Driver->draw2DRectangle(irr::video::SColor(255,130,130,130), rect);
 
 	// client area for background
 	rect = r;
@@ -87,21 +115,22 @@ void IrrRenderer::drawWindowBackgroundIrr(irr::core::rect<s32> r)
 	rect.LowerRightCorner.X -= 2;
 	rect.LowerRightCorner.Y -= 2;
 
-	const video::SColor c1 = getColor(EGDC_WINDOW).getInterpolated ( 0xFFFFFFFF, 0.9f );
-	const video::SColor c2 = getColor(EGDC_WINDOW).getInterpolated ( 0xFFFFFFFF, 0.8f );
+	const irr::video::SColor c1 = irr::video::SColor(255,100,100,100).getInterpolated ( 0xFFFFFFFF, 0.9f );
+	const irr::video::SColor c2 = irr::video::SColor(255,150,150,150).getInterpolated ( 0xFFFFFFFF, 0.8f );
 
-	Driver->draw2DRectangle(rect, c1, c1, c2, c2, clip);
+	Driver->draw2DRectangle(rect, c1, c1, c2, c2);
 }
 
-void IrrRenderer::drawTitleBar(irr::core::rect<s32> r)
+void IrrRenderer::drawTitleBar(irr::core::rect<irr::s32> r)
 {
-	rect = r;
+/*	rect = r;
 	rect.UpperLeftCorner.X += 2;
 	rect.UpperLeftCorner.Y += 2;
 	rect.LowerRightCorner.X -= 2;
 	rect.LowerRightCorner.Y = rect.UpperLeftCorner.Y + getSize(EGDS_WINDOW_BUTTON_WIDTH) + 2;
 
-	const video::SColor c = titleBarColor.getInterpolated( video::SColor(titleBarColor.getAlpha(),255,255,255), 0.8f);
-	Driver->draw2DRectangle(rect, titleBarColor, titleBarColor, c, c, clip);
+	const irr::video::SColor c = titleBarColor.getInterpolated( irr::video::SColor(titleBarColor.getAlpha(),255,255,255), 0.8f);
+	Driver->draw2DRectangle(rect, titleBarColor, titleBarColor, c, c);*/
 }
-}
+} // namespace gui
+} // namespace awrts
--- source/gui/IrrSkin.h
+++ source/gui/IrrSkin.h
@@ -13,15 +13,39 @@
 #include <awengine/gui/Widget.h>
 #include <awengine/gui/Drawer.h>
 
+#include <Irrlicht/rect.h>
+namespace irr {
+namespace video {
+class IVideoDriver;
+}
+}
+
 namespace awrts {
 namespace gui {
+class IrrRenderer {
+public:
+	IrrRenderer(irr::video::IVideoDriver* driver)
+		: Driver(driver)
+	{}
+	void drawWindowBackground(irr::core::rect<irr::s32> r);
+	void drawTitleBar(irr::core::rect<irr::s32> r);
+private:
+	irr::video::IVideoDriver* Driver;
+};
 
-class IrrSkin : Skin {
-}
+class IrrSkin : public Skin {
+public:
+	IrrSkin(irr::video::IVideoDriver* driver)
+		: renderer(driver)
+	{}
+	virtual ~IrrSkin() = default;
+
+	virtual void drawCanvas(Rect<f32> const& rect);
+	virtual void draw3DBorderInset(Rect<f32> const& rect);
+	virtual void drawWindowTitleBar(Rect<f32> const& rect);
+private:
+	IrrRenderer renderer;
+};
 } // namespace gui
 } // namespace awrts
-
-namespace irr {
-class IrrRenderer
-}
 #endif //_awrts_gui_irr_skin_
