c986f0c00a1a4072c8c43a9168a7653911f1db79
Hudd <hededrk@gmail.com>
2015-09-12 09:37:19 +0300

    gui: Drawer: Fix incorrect namespaces

--- source/gui/Drawer.cpp
+++ source/gui/Drawer.cpp
@@ -6,13 +6,16 @@
  * This is free software: you are free to change and redistribute it.
  * There is NO WARRANTY, to the extent permitted by law.
  */
+#include <awengine/utility/macro.h>
+#include <awengine/core/Logger.h>
+#include <awengine/graphics/Color.h>
+
 #include <awengine/gui/Drawer.h>
 #include <awengine/gui/Canvas.h>
 #include <awengine/gui/Widget.h>
 #include <awengine/gui/Window.h>
-#include <awengine/gui/Skin.h>
-#include <awengine/utility/macro.h>
-#include <awengine/core/Logger.h>
+#include <awengine/gui/Engine.h>
+#include <awengine/gui/Style.h>
 namespace awrts {
 namespace gui {
 
@@ -22,26 +25,29 @@ Drawer::~Drawer()
 
 void Drawer::visit(Element* element)
 {
-	Logger::debug("[GUI] Drawer: Unknown Element");
+	core::Logger::debug("[GUI] Drawer: Unknown Element");
 }
+
 void Drawer::visit(Canvas* element)
 {
-	Logger::debug("[GUI] Drawer: Rendering canvas");
+	core::Logger::debug("[GUI] Drawer: Rendering Canvas");
 	//Skin* skin = element->getSkin();
 	//skin->drawCanvas(element->getRect());
 	for (auto& e : element) {
 		e.accept(*this);
 	}
 }
+
 void Drawer::visit(Window* element)
 {
-	Logger::debug("[GUI] Drawer: Rendering Window");
+	core::Logger::debug("[GUI] Drawer: Rendering Window");
 	Skin* skin = element->getSkin();
 	skin->draw3DBorderInset(element->getRect());
 }
+
 void Drawer::visit(Widget* element)
 {
-	Logger::debug("[GUI] Drawer: Unknown Widget");
+	core::Logger::debug("[GUI] Drawer: Unknown Widget");
 }
 
 } // namespace gui
