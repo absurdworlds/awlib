e20d78177b761e53179ab821fa87289a275282c4
Hudd <hededrk@gmail.com>
2015-10-10 19:53:00 +0300

    irr/gui: Update InputManager

--- include/awengine/irr/gui/InputManager.h
+++ include/awengine/irr/gui/InputManager.h
@@ -1,21 +1,20 @@
 /*
  * Copyright (C) 2014  absurdworlds
  *
- * License LGPLv3-only:
+ * License LGPLv3 or later:
  * GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
  * This is free software: you are free to change and redistribute it.
  * There is NO WARRANTY, to the extent permitted by law.
  */
-#ifndef _awrts_InputManager_
-#define _awrts_InputManager_
-
+#ifndef _awrts_InputManager_impl_
+#define _awrts_InputManager_impl_
 #include <forward_list>
 
-#include <Irrlicht/irrlicht.h>
-
-#include <awrts/gui/InputManager.h>
+#include <awengine/gui/InputManager.h>
 
 namespace irr {
+class IrrlichtDevice;
+class SEvent;
 namespace gui {
 class CursorControl;
 }
@@ -23,27 +22,22 @@ class CursorControl;
 
 namespace awrts {
 namespace gui {
-
-/*
- * Class for handling user input. It inherits from irr::IEventReceiver
- * so it can be passed into Irrlicht device as an user event receiver.
- * Then it handles events through OnEvent() callback.
- *
- * To access user input, use GUIManager::getInputManager()
- */
-class InputManager : public InputManager, public irr::IEventReceiver {
+namespace impl {
+class InputManager : public gui::InputManager, public irr::IEventReceiver {
 public: //InputManager
 	InputManager(irr::IrrlichtDevice* device);
-	virtual bool registerReceiver(UserInputReceiver* receiver);
-	virtual bool unregisterReceiver(UserInputReceiver* receiver);
+	virtual bool registerReceiver(EventListener* receiver);
+	virtual bool unregisterReceiver(EventListener* receiver);
 
 public: //irr::IEventReceiver
 	virtual bool OnEvent(const irr::SEvent& event);
 private:
+	irr::IrrlichtDevice* device;
 	irr::gui::ICursorControl* cursor_;
-	std::forward_list<UserInputReceiver*> receivers_;
+	std::forward_list<EventListener*> receivers_;
+	bool convertEvent(const irr::SEvent& irrEvent, Event*& event);
 };
-
+} // namespace impl
 } // namespace io
 } // namespace awrts
-#endif//_awrts_InputManager_
+#endif//_awrts_InputManager_impl_
