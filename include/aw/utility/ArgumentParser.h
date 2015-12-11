f043c44e5c297bd1b4b51625b9407cd54138cf75
Hudd <hededrk@gmail.com>
2015-12-12 00:34:25 +0300

    utility: ArgumentParser: Improve documentation

--- include/aw/utility/ArgumentParser.h
+++ include/aw/utility/ArgumentParser.h
@@ -34,6 +34,7 @@ struct Argument {
 	 * List of types of arguments:
 	 */
 	enum Type : u8 {
+		//! Invalid argument (unused)
 		Invalid,
 		//! Option ('-o') or ('--option')
 		Option,
@@ -45,43 +46,64 @@ struct Argument {
 
 	std::string name;
 
-	//! Returns true if it is a long option
+	//! Returns true if Argument is GNU long option
 	bool longOpt() const
 	{
 		return type == Option && name.size() == 1;
 	}
 };
 
-//! Used to parse command line arguments passed to the program
+/*!
+ * Parses program arguments with POSIX syntax, and additionally supports
+ * GNU long options.
+ *
+ * Here is short summary:
+ * - Options begin with delimiter '`-`';
+ * - Single '`-`' is treated as a non-option argument;
+ * - Some options may take arguments;
+ * - A space is not necessary between an option and an argument;
+ * - Multiple options without arguments can be grouped behind a single '-',
+ *   followed by at most one option-argument;
+ * - Arguments starting with '`--`' are long options. These must be
+ *   separated from other arguments with a space;
+ * - Options may appear in different order or multiple times.
+ *
+ * However, some things are left up to the user of this class:
+ * - This parser doesn't check if options consist of alphanumeric characters,
+ * - It doesn't support '`--option=argument`' syntax,
+ * - Options following '`--`' aren't treated as non-option arguments.
+ */
 class AW_CORE_EXP ArgumentParser {
 public:
 	/*!
 	 * Create an argument parser
 	 * \param argv
-	 *     Array of pointers to command line token strings.
-	 *     (e.g. one that is passed to `main()`)
-	 * \note
-	 *     Each string and array itself must be null-terminated.
+	 *      Pointer to the first element of array of
+	 *      zero-terminated strings.
+	 *      Last element of array msut be nullptr.
 	 */
 	ArgumentParser(char const* const* argv);
 
-	/*
+	/*!
 	 * Get the next argument from the command line
 	 *
-	 * \param tok Object to hold result
 	 * \return
-	 * 	Number of remaining arguments if successful,
-	 * 	When end is reached, 0 is returned.
-	 * 	If error occurs, return value is negative.
+	 *     An \a Argument object or \a nullopt in case
+	 *     there is no more arguments.
 	 */
 	opt<Argument> parseArgument();
 
+	/*!
+	 * Returns either the next parameter, or the rest of
+	 * current argument group. If there are no more
+	 * arguments left, returns empty string
+	 */
 	std::string getParam();
 private:
 	Argument nextArg(char const* arg);
 
 	char const* const* argv;
-	// Prevent modifying pointers
+	// Needed to avoid modifying pointers in argv array
 	char const* args;
 };
 } //namespace core
