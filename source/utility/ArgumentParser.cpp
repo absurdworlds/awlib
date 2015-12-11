7287cc865b9c4a194922fe0399de0df366e3ad7e
Hudd <hededrk@gmail.com>
2015-12-11 18:57:38 +0300

    utility: Rewrite ArgumentParser
    - No longer virtual
    - Uses more complicated algorithm, but doesn't use deque
    - As a result, doesn't parse the whole line beforehand.

--- source/utility/ArgumentParser.cpp
+++ source/utility/ArgumentParser.cpp
@@ -7,87 +7,77 @@
  * This is free software: you are free to change and redistribute it.
  * There is NO WARRANTY, to the extent permitted by law.
  */
-#include "ArgumentParser.h"
+#include <aw/core/ArgumentParser.h>
 
 namespace aw {
 namespace core {
-ArgumentParser* createArgumentParser(char** argv)
+enum ArgumentParser::State : u8 {
+	Default,
+	ArgGroup,
+	ArgsEnd
+};
+
+ArgumentParser::ArgumentParser(char const* const* argv)
+	: state(Default), argv(argv), args(0)
 {
-	return new impl_::ArgumentParser(argv);
 }
 
-namespace impl_ {
-ArgumentParser::ArgumentParser(char** argv)
-	: term_(false)
+Argument ArgumentParser::nextArg(char const* arg)
 {
-	parse(argv);
-}
+	if(*arg != '-')
+		return std::string(arg);
 
-i32 ArgumentParser::getNextArgument(Argument& tok)
-{
-	// bug: because it pops element first, and then returns size,
-	// arg parser returned 0, when there was actually an element
-	// TODO: ^ I think it should just return opt<Argument>
-	// And if needed to check number of arguments left, there should be
-	// separate method
-	size_t size = tokens_.size();
-	if(!tokens_.empty()) {
-		tok = tokens_.back();
-		tokens_.pop_back();
-	}
-	return size;
-}
+	++arg; // Eat '-'
 
-void ArgumentParser::parse(char** argv)
-{
-	while(*argv != 0)
-		parseToken(*argv++);
-}
-
-void ArgumentParser::parseToken (char* argv)
-{
 	Argument tok;
 
-	if(term_ || *argv != '-') {
-		tok.type = Argument::Operand;
-		tok.name = readString(argv);
-		tokens_.push_front(tok);
-		return;
-	}
-
-	++argv;
-
-	if(*argv == 0) {
-		tok.type = Argument::Operand;
-		tok.name = '-';
-	} else if (*argv == '-') {
-		++argv;
-		if(*argv == 0) {
+	switch (*arg) {
+	case 0:
+		return std::string("-");
+	case '-':
+		++arg;
+		if(*arg == 0) {
 			tok.type = Argument::Delim;
 			tok.name = "--";
-			term_ = true;
+			state = ArgsEnd;
 		} else {
 			tok.type = Argument::Option;
-			tok.name = readString(argv);
+			tok.name = std::string(arg);
 			tok.longOpt = true;
 		}
-	} else {
+		break;
+	default:
 		tok.type = Argument::Option;
-		tok.name = *(argv++);
-		while (*argv != 0) {
-			tokens_.push_front(tok);
-			tok.name = *(argv++);
-		}
+		tok.name = *(arg++);
+		state = ArgGroup;
+		args = arg;
 	}
 
-	tokens_.push_front(tok);
+	return tok;
 }
 
-std::string ArgumentParser::readString (char* argv)
+opt<Argument> ArgumentParser::parseArgument()
 {
-	return std::string(argv);
-}
+	if (*argv == 0)
+		return nullopt;
 
-} //namespace impl_
+	switch (state) {
+	case ArgGroup:
+		if (*args != 0) {
+			Argument tok;
+			tok.type = Argument::Option;
+			tok.name = *(args++);
+			return tok;
+		} else {
+			// Reset state and parse next arg
+			// (case fall-through)
+			state = Default;
+		}
+	case Default:
+		return nextArg(*argv++);
+	case ArgsEnd:
+		return Argument(*argv++);
+	};
+}
 } //namespace core
 } //namespace aw
