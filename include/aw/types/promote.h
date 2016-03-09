6d628635f004c3478fac04375b2f46a76f4d63e6
Hudd <hededrk@gmail.com>
2016-03-09 19:24:58 +0300

    types: Add Promote template
    Promote integer to a next larger type

---
+++ include/aw/types/promote.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2014-2016 absurdworlds
+ *
+ * License LGPLv3 or later:
+ * GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
+ * This is free software: you are free to change and redistribute it.
+ * There is NO WARRANTY, to the extent permitted by law.
+ */
+#ifndef aw_types_promote
+#define aw_types_promote
+#include<aw/types/types.h>
+namespace aw {
+namespace detail {
+template <typename T> struct Promote;
+
+template<> struct Promote<i8>  { using type = i16; };
+template<> struct Promote<i16> { using type = i32; };
+template<> struct Promote<i32> { using type = i64; };
+template<> struct Promote<i64> { using type = i64; };
+
+template<> struct Promote<u8>  { using type = u16; };
+template<> struct Promote<u16> { using type = u32; };
+template<> struct Promote<u32> { using type = u64; };
+template<> struct Promote<u64> { using type = u64; };
+
+template<> struct Promote<f32> { using type = f64; };
+template<> struct Promote<f64> { using type = f64; };
+} // namespace detail
+
+//! Promote arithmetic type to a next larger type (if available)
+template <typename T>
+using Promote = typename detail::Promote<T>::type;
+} // namespace aw
+#endif//aw_types_promote
