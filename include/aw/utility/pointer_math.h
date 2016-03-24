fa1536fbabc95eada77a0e0afa3032e247c089c6
Hudd <hededrk@gmail.com>
2016-03-24 16:02:11 +0300

    Utility: Move pointer_math.h into utility

--- include/aw/utility/pointer_math.h
+++ include/aw/utility/pointer_math.h
@@ -1,29 +1,26 @@
 /*
  * Copyright (C) 2014-2015  absurdworlds
  *
- * License LGPLv3-only:
+ * License LGPLv3 or later:
  * GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
  * This is free software: you are free to change and redistribute it.
  * There is NO WARRANTY, to the extent permitted by law.
  */
-#ifndef _aw_pointer_math_
-#define _aw_pointer_math_
-
-#include <aw/common/types.h>
+#ifndef aw_pointer_math_h
+#define aw_pointer_math_h
+#include <aw/types/types.h>
 
 namespace aw {
-
 /*! Align the address to N (power of 2) byte boundry, rounded up */
-inline void* alignUp(void* addr, u8 align)
+inline char* alignUp(char* addr, u8 align)
 {
-	return (void *)(((u8*)(addr) + (u8*)(align - 1)) & ~(u8*)(align - 1));
+	return (addr + static_cast<char*>(align - 1)) & ~static_cast<char*>(align - 1);
 }
 
 /*! Align the address to N (power of 2) byte boundry, rounded down */
-inline void* alignDown(void* addr, u8 align)
+inline char* alignDown(char* addr, u8 align)
 {
-	return (void *)((u8*)(addr) & ~(u8*)(align - 1));
+	return addr & ~static_cast<char*>(align - 1);
 }
-
 } // namespace aw
-#endif//_aw_pointer_math_
+#endif//aw_pointer_math_h
