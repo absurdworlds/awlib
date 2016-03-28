91b0a9ffb1281d23df97097980adc9718cb00b6f
Hudd <hededrk@gmail.com>
2016-03-28 19:13:32 +0300

    utility: Add some array-manipulation functions

---
+++ include/aw/types/support/array.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2016  absurdworlds
+ * Copyright (C) 2016  hedede <haddayn@gmail.com>
+ *
+ * License LGPLv3 or later:
+ * GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
+ * This is free software: you are free to change and redistribute it.
+ * There is NO WARRANTY, to the extent permitted by law.
+ */
+#ifndef aw_types_array_h
+#define aw_types_array_h
+#include <array>
+#include <tuple>
+#include <utility>
+namespace aw {
+namespace detail {
+template<typename T, size_t... I>
+std::array<T,sizeof...(I)+1>
+extend_array(std::array<T,sizeof...(I)>&& array, std::index_sequence<I...>)
+{
+	return {array[I]..., 0};
+}
+
+template<typename L, typename T, size_t... I>
+std::array<L,sizeof...(I)>
+convert_array(std::array<T,sizeof...(I)>&& array, std::index_sequence<I...>)
+{
+	return {array[I]...};
+}
+} // namespace detail
+
+template<typename T, size_t N>
+std::array<T,N+1> extend_array(std::array<T,N>&& array)
+{
+	auto index = std::make_index_sequence<N>{};
+	return detail::extend_array(std::forward<std::array<T,N>>(array), index);
+}
+
+template<typename L, typename T, size_t N>
+std::array<L,N> convert_array(std::array<T,N>&& array)
+{
+	auto index = std::make_index_sequence<N>{};
+	return detail::convert_array<L>(std::forward<std::array<T,N>>(array), index);
+}
+
+template <typename Head, typename...Tail>
+auto make_array(Head arg, Tail...args)
+{
+	std::array<Head, sizeof...(Tail)+1> values = { arg, args... };
+	return values;
+}
+
+namespace detail {
+template <size_t... I, typename T, size_t N>
+auto to_tuple(std::array<T,N> array, std::index_sequence<I...>)
+{
+	return std::make_tuple(array[I]...);
+}
+} // namespace detail
+
+template <typename T, size_t N>
+auto to_tuple(std::array<T,N> array)
+{
+	return detail::to_tuple(array, std::make_index_sequence<N>{});
+}
+} // namespace aw
+#endif//aw_types_array_h
