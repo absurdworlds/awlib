442e0a680c7b46324b81a1b689c243554e6e5984
Hudd <hededrk@gmail.com>
2016-04-10 13:19:26 +0300

    signals: Simplify signal move constructor

--- include/aw/signals/signal.h
+++ include/aw/signals/signal.h
@@ -174,8 +174,7 @@ struct signal<policy, void(Args...)> {
 	 */
 	void disconnect(connection& conn)
 	{
-		auto lock = impl->lock();
-		impl->connections.erase(&conn);
+		impl->remove(&conn);
 	}
 
 	/*!
@@ -235,10 +234,8 @@ private:
 	void move_from(signal&& other)
 	{
 		impl.swap(other.impl);
-		// TODO: store pointer to signal_impl in
-		// connection_impl to make this unnecessary
-		for (auto& conn : impl->connections)
-			conn->sender = this;
+		/* for (auto& conn : impl->connections)
+			conn->sender = this;*/
 	}
 
 	// map<T*, uptr<T>> is used here, as it's easier to
@@ -247,23 +244,37 @@ private:
 	// (extra pointer vs pointer to non-empty deleter)
 	using conn_map = std::map<connection*, std::unique_ptr<connection_type>>;
 
+public:
 	// std::map can be quite large (24 - 56 bytes on different implementations)
 	// and one class can have several signals, so I'm willing to sacrifice
 	// data locality for reduced class size
 	struct signal_impl : policy {
+		void insert(connection_type* conn)
+		{
+			auto lock = this->lock();
+			connections.emplace(conn, connection_ptr(conn));
+		}
+
+		void remove(connection* conn)
+		{
+			auto lock = this->lock();
+			connections.erase(conn);
+		}
+
 		conn_map connections;
 	};
 
+private:
 	std::unique_ptr<signal_impl> impl{new signal_impl};
 };
 
-template<class threading_policy, class T, typename...Args>
+template<class S, class T, typename...Args>
 struct connection_impl : connection_base<Args...> {
 	using base_type = connection_base<Args...>;
 
 	using signature = typename connection_base<Args...>::signature;
 
-	using signal_type = signal<threading_policy, signature>;
+	using signal_type = typename S::signal_impl;
 	using slot_type   = T;
 	using callback_type = member_func<T,signature>;
 
@@ -277,7 +288,7 @@ struct connection_impl : connection_base<Args...> {
 	virtual void disconnect()
 	{
 		if (sender)
-			sender->disconnect(*this);
+			sender->remove(this);
 	}
 
 	virtual void operator()(Args... args) const
@@ -293,7 +304,7 @@ struct connection_impl : connection_base<Args...> {
 
 
 private:
-	friend signal_type;
+	friend S;
 
 	connection_impl(signal_type* sender, T* receiver, callback_type func)
 		: sender(sender), receiver(receiver), callback(func)
@@ -332,14 +343,11 @@ template<class P, typename...Args>
 template<class T>
 connection& signal<P,void(Args...)>::connect(T& obj, member_func<T,void()> func)
 {
-	auto conn = new impl::connection_impl<P,T,Args...>{
-		this, &obj, func
+	auto conn = new impl::connection_impl<signal,T,Args...>{
+		impl.get(), &obj, func
 	};
 
-	{
-		auto lock = impl->lock();
-		impl->connections.emplace(conn, connection_ptr(conn));
-	}
+	impl->insert(conn);
 
 	slot_access::connect(&obj, conn);
 
