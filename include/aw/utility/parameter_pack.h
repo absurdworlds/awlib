0882fc30160d9fcbd662786b600f875901fd30db
Hudd <hededrk@gmail.com>
2016-07-08 01:57:43 +0300

    utility: Add transform_pack
    I don't need it, but I've already wrote it a while ago, after seeing
    article about it and wanting to exercise in template trickery.

--- include/aw/utility/parameter_pack.h
+++ include/aw/utility/parameter_pack.h
@@ -27,5 +27,31 @@ void for_each_index(Func f, index_sequence<Is...>)
 
 template <class T, typename... Ts>
 constexpr bool is_in_pack = bool_or<std::is_same<T,Ts>::value...>;
+
+namespace _impl {
+template<class A, template<class...> class B>
+struct rename_pack;
+
+template<template<class...> class A, class... T, template<class...> class B>
+struct rename_pack<A<T...>, B> {
+	using type = B<T...>;
+};
+} // namespace _impl
+
+template<class A, template<class...> class B>
+using rename_pack = typename _impl::rename_pack<A, B>::type;
+
+namespace _impl {
+template<template<class...> class F, class L>
+struct transform_pack;
+
+template<template<class...> class F, template<class...> class L, class... T>
+struct transform_pack<F, L<T...>> {
+	using type = L<F<T>...>;
+};
+} // namespace _impl
+
+template<template<class...> class F, class L>
+using transform_pack = typename _impl::transform_pack<F, L>::type;
 } // namespace aw
 #endif//aw_parameter_pack_h
