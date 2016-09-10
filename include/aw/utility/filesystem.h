0e2390ddec59b817916d150d454cf0679e6cbc00
Hudd <hededrk@gmail.com>
2016-09-10 22:17:07 +0300

    utility: Add filesystem.h

---
+++ include/aw/utility/filesystem.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2016  Hedede <hededrk@gmail.com>
+ *
+ * License LGPLv3 or later:
+ * GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
+ * This is free software: you are free to change and redistribute it.
+ * There is NO WARRANTY, to the extent permitted by law.
+ */
+#ifndef aw_utility_filesystem_h
+#define aw_utility_filesystem_h
+#if __has_include(<filesystem>)
+#include <filesystem>
+#elif __has_include(<experimental/filesystem>)
+#include <experimental/filesystem>
+#endif
+#include <aw/config.h>
+namespace aw {
+namespace filesystem {
+#ifdef __cpp_lib_experimental_filesystem
+#if (AW_COMPILER == AW_COMPILER_MSVC)
+using namespace std::tr2::filesystem;
+#else
+using namespace std::experimental::filesystem;
+#endif
+#else
+using namespace std::filesystem;
+#endif
+} // namespace filesystem
+using namespace filesystem = fs;
+} // namespace aw
+#endif//aw_utility_filesystem_h
