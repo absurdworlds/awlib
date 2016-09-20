e884506311db1eec156c6185351f98cd6f57c08e
Hudd <hededrk@gmail.com>
2016-09-20 06:51:35 +0300

    io: Add invalid_fd

--- include/aw/io/file_descriptor.h
+++ include/aw/io/file_descriptor.h
@@ -8,24 +8,30 @@
  */
 #ifndef aw_io_file_descriptor_h
 #define aw_io_file_descriptor_h
+#include <aw/config.h>
 namespace aw {
 namespace io {
 #if   (AW_PLATFORM == AW_PLATFORM_POSIX)
 namespace posix {
 using file_descriptor = int;
+constexpr file_descriptor invalid_fd = file_descriptor(-1);
 } // namespace posix
 #elif (AW_PLATFORM == AW_PLATFORM_WIN32)
 namespace win32 {
-using file_descriptor = intptr_t;
+using file_descriptor = void*;
+constexpr file_descriptor invalid_fd = file_descriptor(-1);
 } // namespace win32
 #endif
 
 #if (AW_PLATFORM == AW_PLATFORM_POSIX)
 using posix::file_descriptor;
+using posix::invalid_fd;
 #elif (AW_PLATFORM == AW_PLATFORM_WIN32)
 using win32::file_descriptor;
+using win32::invalid_fd;
 #else
 using file_descriptor = int;
 #endif
 } // namespace io
 } // namespace aw
+#endif//aw_io_file_descriptor_h
