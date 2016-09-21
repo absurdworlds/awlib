a999388059341503c1e5a67c88470f6544429163
Hudd <hededrk@gmail.com>
2016-09-21 21:06:58 +0300

    test: Fix 'unused' warning

--- source/test/always_fail.cpp
+++ source/test/always_fail.cpp
@@ -10,7 +10,7 @@ Test(always_fail3) { Checks { TestEqualV(1, 2, 2); } }
 Test(always_fail4) { Checks { TestEqualV(1, 2, 3); } }
 Test(always_fail5) { Checks { throw "what is this"; } }
 Test(always_fail6) { Checks { throw std::logic_error("you suck"); } }
-Test(always_fail7) { Checks { TestCatch(std::logic_error, 1+2); } }
+Test(always_fail7) { Checks { TestCatch(std::logic_error, (void)0); } }
 Test(always_fail8) { Checks { TestCatch(std::logic_error, throw 2); } }
 Test(always_fail9) { Checks { TestCatch(std::logic_error, throw std::runtime_error{"a"}); } }
 Test(always_faila) { Checks { TestEqual(1, 2); } }
