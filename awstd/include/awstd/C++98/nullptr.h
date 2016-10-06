68846c4960554844175f4843086c1f952b82f81d
Hudd <haddayn@gmail.com>
2016-10-06 21:27:44 +0300

    Improve nullptr
    (based on
    https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/nullptr)

--- awstd/include/awstd/C++98/nullptr.h
+++ awstd/include/awstd/C++98/nullptr.h
@@ -4,15 +4,24 @@
 #undef nullptr
 #endif
 
-static struct nullptr_t {
+static const struct nullptr_t {
 	template<typename T>
-	operator T*() { return reinterpret_cast<T*>(0); }
-} nullptr;
+	operator T*() const { return 0; }
+
+	template<class C, class T>
+	operator T C::*() const { return 0; }
+
+	// for sizeof()
+	void const* const _dummy_;
+
+private:
+	void operator&() const;
+} nullptr = {0};
 
 
 #ifdef NULL
 #undef NULL
 #endif
 
-#define NULL nullptr;
+#define NULL nullptr
 #endif//awstd_nullptr_h
