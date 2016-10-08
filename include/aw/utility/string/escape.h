3823647cb61de3dd56f1151a334283ae67a900e3
Hudd <hededrk@gmail.com>
2016-10-08 23:19:35 +0300

    string: Add espace_quotes()

---
+++ include/aw/utility/string/escape.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2016  absurdworlds
+ * Copyright (C) 2016  Hedede <haddayn@gmail.com>
+ *
+ * License LGPLv3 or later:
+ * GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
+ * This is free software: you are free to change and redistribute it.
+ * There is NO WARRANTY, to the extent permitted by law.
+ */
+#ifndef aw_string_escape_h
+#define aw_string_escape_h
+#include <string>
+namespace aw {
+namespace string {
+std::string& escape_quotes(std::string const& src, std::string& dest)
+{
+	dest.reserve(dest.size() + src.size());
+	for (char c : src) switch (c) {
+	case '\\':
+	case '"':
+		dest.push_back('\\');
+		[[fallthrough]];
+	default:
+		dest.push_back(c);
+	}
+	return dest;
+}
+
+std::string escape_quotes(std::string const& src)
+{
+	std::string dest;
+	escape_quotes(src, dest);
+	return dest;
+}
+} // namespace string
+} // namespace aw
+#endif//aw_string_escape_h
