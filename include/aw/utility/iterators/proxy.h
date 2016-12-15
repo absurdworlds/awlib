74c6dbd413bdd06f3f9650d3ec1a7f24c1fe8767
Hudd <haddayn@gmail.com>
2016-12-15 18:43:58 +0300

    utility/iterators: Add proxy
    Again, I forgot to add it. I need a better workflow.

---
+++ include/aw/utility/iterators/proxy.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2016  hedede <haddayn@gmail.com>
+ *
+ * License LGPLv3 or later:
+ * GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
+ * This is free software: you are free to change and redistribute it.
+ * There is NO WARRANTY, to the extent permitted by law.
+ */
+#ifndef aw_iterators_proxy_h
+#define aw_iterators_proxy_h
+namespace aw {
+namespace iter {
+/*!
+ * Helper class for use with overloaded operator->(),
+ */
+template <typename T>
+struct proxy {
+	T _temp;
+
+	constexpr T* operator->()
+	{
+		return &_temp;
+	}
+
+	constexpr T const* operator->() const
+	{
+		return &_temp;
+	}
+};
+} // namespace iter
+} // namespace aw
+#endif//aw_IteratorWrapper_h
