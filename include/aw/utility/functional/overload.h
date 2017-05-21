3282f380be90f414851ff64b9815a36eee8b5473
Hudd <haddayn@gmail.com>
2017-05-22 00:30:21 +0300

    utility: Update overload() to C++17

--- include/aw/utility/functional/overload.h
+++ include/aw/utility/functional/overload.h
@@ -11,41 +11,38 @@
 #define aw_utility_functional_overload_h
 #include <utility>
 namespace aw {
-template<typename Func, typename...Fs>
-struct overloaded : overloaded<Func>, overloaded<Fs...> {
-	overloaded(Func&& func, Fs&&... fs)
-		: overloaded<Func>{std::forward<F>(func)},
-		  overloaded<Fs...>{std::forward<Fs>(fs)...}
-	{}
-
-	using overloaded<Func>::operator();
-	using overloaded<Fs...>::operator();
-};
-
-template<typename Func>
-struct overloaded<Func> : Func {
-	overloaded(Func&& func)
-		: Func{std::forward<Func>(func)}
-	{}
-};
-
-// TODO: wait for P0195R1 implementation
-// (will replace this whole header)
-#if 0
+#if __cpp_variadic_using >= 201611
+/*!
+ * Allows to combine multiple functors into overloaded set.
+ */
 template<typename...Fs>
 struct overload : Fs... {
 	overload(Fs... fs) : Fs{ std::move(fs) }...  {}
 	using Fs::operator()...;
 };
+#else
+template<typename Func, typename...Fs>
+struct overload : overload<Func>, overload<Fs...> {
+	overloaded(Func func, Fs... fs)
+		: overload<Func>{std::forward<F>(func)},
+		  overload<Fs...>{std::forward<Fs>(fs)...}
+	{}
+
+	using overload<Func>::operator();
+	using overload<Fs...>::operator();
+};
+
+template<typename Func>
+struct overload<Func> : Func {
+	overload(Func func)
+		: Func{std::forward<Func>(func)}
+	{}
+};
 #endif
 
-
-/*!
- * Allows to combine multiple functors into
- * overloaded set of functors.
- */
 template<typename...Fs>
-overloaded<Fs> overload(Fs&&...fs)
+[[deprecated]]
+overload<Fs...> make_overload(Fs&&...fs)
 {
 	return {std::forward<Fs>(fs)...};
 }
