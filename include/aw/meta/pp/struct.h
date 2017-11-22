9060cda4c7314fb15c78a3ea0de474c0795b6c39
Hudd <haddayn@gmail.com>
2017-11-23 00:53:12 +0300

    macro: Separate DEFINE_STRUCT into multiple macros

--- include/aw/meta/pp/struct.h
+++ include/aw/meta/pp/struct.h
@@ -11,16 +11,28 @@
 #include "for_each.h"
 #include "tuple.h"
 
+#define DEFINE_STRUCT_MEMBERS(NAME,...)\
+	FOR_EACH(SEP_SEMI, SEP_SPACE DEFER, __VA_ARGS__ )
+#define DEFINE_STRUCT_TYPES(NAME,...)\
+	FOR_EACH( SEP_COMMA, FIRST DEFER, __VA_ARGS__ )
+#define DEFINE_STRUCT_TUPLE_TYPE(NAME,...)\
+	using tuple_type = std::tuple< DEFINE_STRUCT_TYPES(__VA_ARGS__) >
+#define DEFINE_STRUCT_TUPLE_OPERATOR(NAME,...)\
+	constexpr operator tuple_type() { \
+		return { DEFINE_STRUCT_NAMES( __VA_ARGS__ ) };\
+	}
+#define DEFINE_STRUCT_NAMES(NAME,...)\
+	FOR_EACH( SEP_COMMA, SECOND DEFER, __VA_ARGS__ )
+#define DEFINE_STRUCT_NAME_STRINGS(NAME,...)\
+	FOR_EACH( SEP_COMMA, SECOND DEFER, __VA_ARGS__ )
+	
+
 #define DEFINE_STRUCT(NAME,...) \
 struct NAME { \
-	FOR_EACH(SEP_SEMI, SEP_SPACE DEFER, __VA_ARGS__ ); \
-	using tuple_type = std::tuple< \
-		FOR_EACH( SEP_COMMA, FIRST DEFER, __VA_ARGS__ ) \
-	>; \
-	constexpr operator tuple_type() \
-	{ \
-		return { FOR_EACH( SEP_COMMA, SECOND DEFER, __VA_ARGS__ ) }; \
-	} \
+	DEFINE_STRUCT_MEMBERS(NAME,__VA_ARGS__);\
+	DEFINE_STRUCT_TUPLE_TYPE(NAME,__VA_ARGS__);\
+	DEFINE_STRUCT_TUPLE_OPERATOR(NAME,__VA_ARGS__);\
 }
+
 
 #endif//aw_meta_pp_struct_h
