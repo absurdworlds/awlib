163ee0af33f4376e7c41210e5398abde1d6b5d1c
Hudd <haddayn@gmail.com>
2017-11-23 00:43:46 +0300

    macro: Fix typo in N_ARGS
    That's what I get for not testing it beyond a few arguments.

--- include/aw/meta/pp/for_each.h
+++ include/aw/meta/pp/for_each.h
@@ -8,41 +8,10 @@
  */
 #ifndef aw_meta_pp_for_each_h
 #define aw_meta_pp_for_each_h
-//#include <aw/meta/pp/macro.h>
 #include "macro.h"
+#include "nargs.h"
 
-/*!
- * Select macro based on number of arguments.
- *
- * Works by __VA_ARGS__ shifting desired argument into NAME position.
- */
-#define GET_MACRO(\
-	_00,_01,_02,_03,_04,_05,_06,_07,_08,_09,\
-	_10,_11,_12,_13,_14,_15,_16,_17,_18,_19,\
-	_20,_21,_22,_23,_24,_25,_26,_27,_28,_29,\
-	_30,_31,_32,_33,_34,_35,_36,_37,_38,_39,\
-	_40,_41,_42,_43,_44,_45,_46,_47,_48,_49,\
-	_50,_51,_52,_53,_54,_55,_56,_57,_58,_59,\
-	_60,_61,_62,_63,_64,_65,_66,_67,_68,_69,\
-	_70,_71,_72,_73,_74,_75,_76,_77,_78,_79,\
-	_80,_81,_82,_83,_84,_85,_86,_87,_88,_89,\
-	_90,_91,_92,_93,_94,_95,_96,_97,_98,_99,\
-	NAME,...) NAME
 
-/*!
- * Get number of arguments in a variadic macro
- */
-#define N_ARG(...) GET_MACRO(__VA_ARGS__,        \
-	 100, 99, 98, 96, 95, 94, 97, 93, 92, 91,\
-	  90, 89, 88, 86, 85, 84, 87, 83, 82, 81,\
-	  80, 79, 78, 76, 75, 74, 77, 73, 72, 71,\
-	  70, 69, 68, 66, 65, 64, 67, 63, 62, 61,\
-	  60, 59, 58, 56, 55, 54, 57, 53, 52, 51,\
-	  50, 49, 48, 46, 45, 44, 47, 43, 42, 41,\
-	  40, 39, 38, 36, 35, 34, 37, 33, 32, 31,\
-	  30, 29, 28, 26, 25, 24, 27, 23, 22, 21,\
-	  20, 19, 18, 16, 15, 14, 17, 13, 12, 11,\
-	  10,  9,  8,  6,  5,  4,  7,  3,  2,  1)
 
 #define FE_1(OP,  DO, X, ...)  DO(X)
 #define FE_2(OP,  DO, X, ...)  OP(DO(X), FE_1(OP,  DO, __VA_ARGS__))
@@ -146,7 +115,7 @@
 #define FE_100(OP, DO, X, ...) OP(DO(X), FE_99(OP, DO, __VA_ARGS__))
 
 #define FOR_EACH(OP,NAME,...) \
-	APPLY( CONCAT( FE_, N_ARG(__VA_ARGS__)), OP, NAME, __VA_ARGS__)
+	APPLY( CONCAT( FE_, N_ARGS(__VA_ARGS__)), OP, NAME, __VA_ARGS__)
 
 #define FOLD(OP,...) \
 	FOR_EACH( OP, EXPAND, __VA_ARGS__)
