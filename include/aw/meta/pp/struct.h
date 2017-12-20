1a6d0c22f80581506a9d7c788baeb82e1bc7323e
Hudd <haddayn@gmail.com>
2017-12-20 22:47:50 +0300

    pp: Add AW_ prefix to struct macros

--- include/aw/meta/pp/struct.h
+++ include/aw/meta/pp/struct.h
@@ -8,31 +8,53 @@
  */
 #ifndef aw_meta_pp_struct_h
 #define aw_meta_pp_struct_h
+#include "macro.h"
 #include "foldr.h"
 #include "tuple.h"
 
-#define DEFINE_STRUCT_MEMBERS(NAME,...)\
-	FOLD_RIGHT(SEP_SEMI, SEP_SPACE DEFER, __VA_ARGS__ )
-#define DEFINE_STRUCT_TYPES(NAME,...)\
+#include <tuple>
+#include <aw/meta/void_t.h>
+/*!
+ * A function-macro to turn preprocessor "tuple" (TYPE,NAME) into a
+ * variable declaration.
+ *
+ * type_t is required to correctly handle pesky c-style arrays and function pointers:
+ *
+ * `AW_DECLARE_VAR(char[8], name);` will expand to:
+ * without type_t  ---> `char[8]         name;` // ERROR
+ * with    type_t  ---> `type_t<char[8]> name;` // OK, equivalent to `char name[8]`
+ */
+#define AW_DECLARE_VAR(TYPE,NAME) ::aw::type_t<TYPE> NAME
+
+/*!
+ * Turns a list of preprocessor "tuples" into list of variable declarations
+ */
+#define AW_DEFINE_STRUCT_MEMBERS(NAME,...)\
+	FOLD_RIGHT( SEP_SEMI, AW_DECLARE_VAR DEFER, __VA_ARGS__ )
+/*!
+ * Expands list of preprocessor "tuples" (type, name)
+ * into comma-separated list of types
+ */
+#define AW_LIST_MEMBER_TYPES(NAME,...)\
 	FOLD_RIGHT( SEP_COMMA, FIRST DEFER, __VA_ARGS__ )
-#define DEFINE_STRUCT_TUPLE_TYPE(NAME,...)\
-	using tuple_type = std::tuple< DEFINE_STRUCT_TYPES(__VA_ARGS__) >
-#define DEFINE_STRUCT_TUPLE_OPERATOR(NAME,...)\
-	constexpr operator tuple_type() { \
-		return { DEFINE_STRUCT_NAMES( __VA_ARGS__ ) };\
-	}
-#define DEFINE_STRUCT_NAMES(NAME,...)\
+#define AW_LIST_MEMBER_NAMES(NAME,...)\
 	FOLD_RIGHT( SEP_COMMA, SECOND DEFER, __VA_ARGS__ )
-#define DEFINE_STRUCT_NAME_STRINGS(NAME,...)\
+#define AW_DEFINE_STRUCT_TUPLE_TYPE(NAME,...)\
+	using tuple_type = std::tuple< AW_LIST_MEMBER_TYPES(__VA_ARGS__) >
+#define AW_DEFINE_STRUCT_TUPLE_OPERATOR(NAME,...)\
+	constexpr operator tuple_type() { \
+		return { AW_LIST_MEMBER_NAMES( __VA_ARGS__ ) };\
+	}
+
+#define AW_LIST_MEMBER_NAME_STRINGS(NAME,...)\
 	FOLD_RIGHT( SEP_COMMA, SECOND DEFER, __VA_ARGS__ )
 	
 
-#define DEFINE_STRUCT(NAME,...) \
+#define AW_DEFINE_STRUCT(NAME,...) \
 struct NAME { \
-	DEFINE_STRUCT_MEMBERS(NAME,__VA_ARGS__);\
-	DEFINE_STRUCT_TUPLE_TYPE(NAME,__VA_ARGS__);\
-	DEFINE_STRUCT_TUPLE_OPERATOR(NAME,__VA_ARGS__);\
+	AW_DEFINE_STRUCT_MEMBERS(NAME,__VA_ARGS__);\
+	AW_DEFINE_STRUCT_TUPLE_TYPE(NAME,__VA_ARGS__);\
+	AW_DEFINE_STRUCT_TUPLE_OPERATOR(NAME,__VA_ARGS__);\
 }
-
 
 #endif//aw_meta_pp_struct_h
