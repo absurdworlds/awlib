5f58d2768ba209b1c82d19ec14783523b62f6ba5
Hudd <haddayn@gmail.com>
2018-01-07 03:08:32 +0300

    io: Preserve error code in mmap_file ctor
    When file fails to open, it produces an error_code. Before this
    commit, mmap_file didn't check the error_code and overrode it with
    "ERROR_INVALID_HANDLE".

--- include/aw/io/mmap_file.h
+++ include/aw/io/mmap_file.h
@@ -119,7 +119,8 @@ struct mmap_file {
 	mmap_file(file_descriptor fd, std::error_code& ec, map_perms perms = map_perms::rdwr)
 		: _file{fd}
 	{
-		create_mapping( ec, perms );
+		if (!ec)
+                      create_mapping( ec, perms );
 	}
 
 	mmap_file(fs::path const& path, map_perms perms = map_perms::rdwr)
@@ -130,9 +131,10 @@ struct mmap_file {
 	}
 
 	mmap_file(fs::path const& path, std::error_code& ec, map_perms perms = map_perms::rdwr)
-		: _file{path, get_file_mode(perms)}
+		: _file{path, get_file_mode(perms), ec}
 	{
-		create_mapping( ec, perms );
+		if (!ec)
+			create_mapping( ec, perms );
 	}
 
 	~mmap_file()
