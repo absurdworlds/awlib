578af933803fe934ed8644bbb19ace098748c76b
Hudd <haddayn@gmail.com>
2018-02-10 01:20:29 +0300

    types: Add circular_buffer
    A fixed-size buffer that overwrites itself (FIFO). This is useful for
    various purposes, for example, for a fixed-size log, which holds last N
    messages.

---
+++ include/aw/types/containers/circular_buffer.h
@@ -0,0 +1,470 @@
+/*
+ * Copyright (C) 2016  hedede <haddayn@gmail.com>
+ *
+ * License LGPLv3 or later:
+ * GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
+ * This is free software: you are free to change and redistribute it.
+ * There is NO WARRANTY, to the extent permitted by law.
+ */
+#ifndef aw_containers_circular_buffer_h
+#define aw_containers_circular_buffer_h
+#include <cassert>
+#include <memory>
+#include <iterator>
+#include <algorithm>
+namespace aw {
+// TODO: move bits shared with queue into common header
+namespace _impl {
+template <typename Traits>
+struct traits {
+	using value_type        = typename Traits::value_type;
+	using pointer           = typename Traits::pointer;
+	using reference         = typename Traits::reference;
+	using difference_type   = typename Traits::difference_type;
+};
+
+template <typename Traits>
+struct const_traits {
+	using value_type        = typename Traits::value_type;
+	using pointer           = typename Traits::const_pointer;
+	using reference         = typename Traits::const_reference;
+	using difference_type   = typename Traits::difference_type;
+};
+
+template<typename T>
+struct cbuffer_data {
+	using pointer = T*;
+	using difference_type = ptrdiff_t;
+
+	pointer begin = nullptr;
+	pointer end   = nullptr;
+	pointer head  = nullptr;
+	pointer tail  = nullptr;
+
+	pointer next_p(pointer p) const noexcept
+	{
+		if (p + 1 == end)
+			return begin;
+		return p + 1;
+	}
+
+	pointer prev_p(pointer p) const noexcept
+	{
+		if (p == begin)
+			return end - 1;
+		return p - 1;
+	}
+
+	pointer add_p(pointer p, difference_type n) const noexcept
+	{
+		if (n < end - p)
+			return p + n;
+
+		return p + n - end + begin;
+	}
+
+	pointer sub_p(pointer p, difference_type n) const noexcept
+	{
+		if (n <= p - begin)
+			return p - n;
+		return p - n + end - begin;
+	}
+
+	pointer map_p(pointer p) const noexcept
+	{
+		if (p < head)
+			return p + (end - head);
+		return begin + (p - head);
+	}
+};
+
+template<typename Traits>
+struct circular_iterator {
+	using value_type        = typename Traits::value_type;
+	using pointer           = typename Traits::pointer;
+	using reference         = typename Traits::reference;
+	using difference_type   = typename Traits::difference_type;
+	using iterator_category = std::random_access_iterator_tag;
+
+	using iterator = circular_iterator;
+
+	circular_iterator() noexcept = default;
+
+	circular_iterator(pointer p, cbuffer_data<value_type> data) noexcept
+		: data{data}, p{p}
+	{
+	}
+
+	circular_iterator(iterator const& other) noexcept
+		: data{other.data}, p{other.p}
+	{
+	}
+
+	iterator& operator=(iterator const& other) noexcept
+	{
+		data = other.data;
+		p = other.p;
+	}
+
+	reference operator*() const noexcept
+	{
+		return *p;
+	}
+
+	pointer operator->() const noexcept
+	{
+		return &(operator*());
+	}
+
+	iterator& operator--() noexcept
+	{
+		p = data.prev_p(p);
+		return *this;
+	}
+
+	iterator& operator++() noexcept
+	{
+		p = data.next_p(p);
+		return *this;
+	}
+
+	bool operator==(iterator const& other) const noexcept
+	{
+		//return (map() == other.map());
+		return p == other.p;
+	}
+
+	bool operator!=(iterator const& other) const noexcept
+	{
+		//return (map() != other.map());
+		return p != other.p;
+	}
+
+private:
+	cbuffer_data<value_type> data;
+	pointer p = nullptr;
+};
+} // namespace _impl
+
+/*!
+ * circular_buffer is a fixed-size buffer, which overwrites itself
+ * when it is full.
+ *
+ * Does not support allocators.
+ * TODO: do we need allocator support?
+ */
+template<typename T>
+class circular_buffer {
+public:
+	using value_type       = T;
+	using reference        = T&;
+	using const_reference  = T const&;
+	using size_type        = size_t;
+	using difference_type  = ptrdiff_t;
+	using pointer          = T*;
+	using const_pointer    = T const*;
+	using iterator         = _impl::circular_iterator<_impl::traits<circular_buffer>>;
+	using const_iterator   = _impl::circular_iterator<_impl::const_traits<circular_buffer>>;
+	using reverse_iterator       = std::reverse_iterator<iterator>;
+	using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+private:
+	_impl::cbuffer_data<T> data;
+
+	void create_storage(size_type size)
+	{
+		++size; // for null element
+		data.begin = (pointer)operator new(size);
+		data.end   = data.begin + size;
+		data.head  = data.tail = data.begin;
+	}
+
+	void destroy_storage()
+	{
+		delete[] data.begin;
+	}
+
+public:
+
+	/*! Create buffer with specified capacity */
+	explicit circular_buffer(size_type size)
+	{
+		create_storage( size );
+	}
+
+	/*!
+	 * Copy constructor.
+	 * Creates buffer of the same size and copies all elements.
+	 */
+	circular_buffer(circular_buffer const& q)
+		: circular_buffer( q.size() )
+	{
+		std::uninitialized_copy(q.begin(), q.end(), begin());
+	}
+
+	/*! TODO: move constructor */
+
+
+	~circular_buffer()
+	{
+		destroy(begin(), end());
+		destroy_storage();
+	}
+
+
+	/*! Iterator to the front of queue */
+	iterator begin()
+	{
+		return {data.head, data};
+	}
+
+	/*! Iterator to the front of queue */
+	const_iterator begin() const
+	{
+		return {data.head, data};
+	}
+
+	/*! Iterator to the front of queue */
+	const_iterator cbegin() const
+	{
+		return {begin()};
+	}
+
+	/*! Iterator to element past the end of queue */
+	iterator end()
+	{
+		return {data.tail, data};
+	}
+
+	/*! Iterator to element past the end of queue */
+	const_iterator end() const
+	{
+		return {data.tail, data};
+	}
+
+	/*! Iterator to element past the end of queue */
+	const_iterator cend() const
+	{
+		return {end()};
+	}
+
+	/*! Reverse iterator to the end of queue */
+	reverse_iterator rbegin()
+	{
+		return {end()};
+	}
+
+
+	/*! Reverse iterator to the end of queue */
+	const_reverse_iterator rbegin() const
+	{
+		return {end()};
+	}
+
+	/*! Reverse iterator to the end of queue */
+	const_reverse_iterator crbegin() const
+	{
+		return {cend()};
+	}
+
+	/*! Reverse iterator to start of queue */
+	reverse_iterator rend()
+	{
+		return {begin()};
+	}
+
+	/*! Reverse iterator to start of queue */
+	const_reverse_iterator rend() const
+	{
+		return {begin()};
+	}
+
+	/*! Reverse iterator to start of queue */
+	const_reverse_iterator crend() const
+	{
+		return {cbegin()};
+	}
+
+	/*! Get number of elements in queue */
+	size_type size() const noexcept
+	{
+		auto dist = end() - begin();
+		return dist - 1;
+	}
+
+	/*! Get buffer capacity */
+	size_type capacity() const noexcept
+	{
+		auto dist = data.end - data.begin;
+		return dist - 1;
+	}
+
+	/*!
+	 * Erases all elements in queue.
+	 *
+	 * \note
+	 * It does not free allocated memory.
+	 * If elements are pointers, memory pointed-to by them
+	 * is not freed.
+	 */
+	void clear()
+	{
+		destroy(begin(), end());
+		data.tail = data.head;
+	}
+
+	/*!
+	 * Check if queue is empty
+	 */
+	bool empty() const noexcept
+	{
+		return begin() == end();
+	}
+
+	/*!
+	 * Destroy element at the front of queue
+	 */
+	void pop_front()
+	{
+		assert(!empty() && "pop_front() on empty queue.");
+		data.head->~T();
+		data.head = data.next_p(data.head);
+	}
+
+	/*!
+	 * Destroy element at the back of queue
+	 */
+	void pop_back()
+	{
+		assert(!empty() && "pop_back() on empty queue.");
+		data.tail = data.prev_p(data.tail);
+		data.tail->~T();
+	}
+
+	/*!
+	 * Insert element at the front of queue
+	 */
+	void push_front(const_reference val)
+	{
+		emplace_front(val);
+	}
+
+	/*!
+	 * Insert element at the end of queue
+	 */
+	void push_back(const_reference val)
+	{
+		emplace_back(val);
+	}
+
+	/*!
+	 * Insert element at the front of queue
+	 */
+	void push_front(value_type&& val)
+	{
+		emplace_front(std::move(val));
+	}
+
+	/*!
+	 * Insert element at the end of queue
+	 */
+	void push_back(value_type&& val)
+	{
+		emplace_back(std::move(val));
+	}
+
+	/*!
+	 * Construct element at the front of queue
+	 */
+	template <typename... Args>
+	void emplace_front(Args&&... args)
+	{
+		auto head = data.prev_p(data.head);
+		if (head == data.tail)
+			pop_back();
+		data.head = head;
+		construct(data.head, std::forward<Args>(args)...);
+	}
+
+	/*!
+	 * Construct element at the end of queue
+	 */
+	template <typename... Args>
+	void emplace_back(Args&&... args)
+	{
+		construct(data.tail, std::forward<Args>(args)...);
+		auto tail = data.next_p(data.tail);
+		if (tail == data.head)
+			pop_front();
+		data.tail = tail;
+	}
+
+	/*! Get element at the head of queue */
+	const_reference front() const noexcept
+	{
+		assert(!empty() && "front() on empty queue.");
+		return *begin();
+	}
+
+	/*! Get element at the head of queue */
+	reference front() noexcept
+	{
+		assert(!empty() && "front() on empty queue.");
+		return *begin();
+	}
+
+	/*! Get element at the tail of queue */
+	const_reference back() const noexcept
+	{
+		assert(!empty() && "back() on empty queue.");
+		return *(end() - 1);
+	}
+
+	/*! Get element at the tail of queue */
+	reference back() noexcept
+	{
+		assert(!empty() && "back() on empty queue.");
+		return *(end() - 1);
+	}
+
+	const_reference operator[](size_type n) const noexcept
+	{
+		assert(n < size() && "Index out of bounds.");
+		return begin()[n];
+	}
+
+	reference operator[](size_type n) noexcept
+	{
+		assert(n < size() && "Index out of bounds.");
+		return begin()[n];
+	}
+
+private:
+	template <typename... Args>
+	void construct(pointer p, Args&&... args)
+	{
+		new (p) T( std::forward<Args>(args)... );
+	}
+
+	void destroy(iterator head, iterator tail)
+	{
+		for (; head != tail; ++head)
+			head->~T();
+	}
+
+
+	template<typename Iterator, typename Sentinel>
+	void range_init(Iterator first, Sentinel last, std::input_iterator_tag)
+	{
+		for (; first != last; ++first)
+			emplace_back(*first);
+	}
+
+	template<typename Iterator, typename Sentinel>
+	void range_init(Iterator first, Sentinel last, std::forward_iterator_tag)
+	{
+		data.tail = std::uninitialized_copy(first, last, data.head);
+	}
+};
+} // namespace aw
+#endif//aw_containers_circular_buffer_h
---
+++ source/types/tests/circular_buffer.c++
@@ -0,0 +1,56 @@
+#include <array>
+#include <aw/types/containers/circular_buffer.h>
+
+#include <aw/utility/ranges/zip.h>
+#include <aw/utility/test.h>
+
+TestFile( "aw::circular_buffer" );
+
+namespace aw {
+Test(circular_buf_push_back) {
+	circular_buffer<int> buf(3);
+	buf.push_back(1);
+	buf.push_back(2);
+	buf.push_back(3);
+
+	Checks {
+		constexpr std::array prot{1,2,3};
+		for (auto [e,v] : zip(prot, buf))
+			TestEqual(e,v);
+	}
+
+	buf.push_back(4);
+	Checks {
+		constexpr std::array prot{2,3,4};
+		for (auto [e,v] : zip(prot, buf))
+			TestEqual(e,v);
+	}
+}						
+
+Test(circular_buf_push_front) {
+	circular_buffer<int> buf(3);
+	buf.push_front(1);
+	buf.push_front(2);
+	buf.push_front(3);
+
+	Checks {
+		constexpr std::array prot{3,2,1};
+		for (auto [e,v] : zip(prot, buf))
+			TestEqual(e,v);
+	}
+
+	buf.push_front(4);
+	Checks {
+		constexpr std::array prot{4,3,2};
+		for (auto [e,v] : zip(prot, buf))
+			TestEqual(e,v);
+	}
+
+	buf.push_back(5);
+	Checks {
+		constexpr std::array prot{4,3,5};
+		for (auto [e,v] : zip(prot, buf))
+			TestEqual(e,v);
+	}
+}
+} // namespace aw
