c73531c1d08110c58edeaeb42eeb45cf8033a7b7
Hudd <haddayn@gmail.com>
2019-03-14 12:48:55 +0300

    log: Add forward-declarations header
    Like standard <iosfwd>. It is kinda annoying to forward-declare log,
    because of inline namespace (why, oh why did I decide to version
    stuff?), but the full header is needed only in very few places (even
    though it's small, I don't like having unneeded stuff)

---
+++ include/aw/log/log_fwd.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C)      2016  Hedede <haddayn@gmail.com>
+ * Copyright (C) 2014-2016  absurdworlds
+ *
+ * License LGPLv3 or later:
+ * GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
+ * This is free software: you are free to change and redistribute it.
+ * There is NO WARRANTY, to the extent permitted by law.
+ */
+#ifndef aw_log_log_fwd_h
+#define aw_log_log_fwd_h
+/*
+ * This file contains forward-declarations of logger interfaces
+ */
+namespace aw {
+inline namespace v1 {
+struct log;
+} // namespace v1
+} // namespace aw
+#endif//aw_log_log_fwd_h
