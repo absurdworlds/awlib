9246620997be73210249d7af3c19d7a09dc73733
Hudd <haddayn@gmail.com>
2020-07-26 06:33:16 +0300

    test: Adjust output for xUnit

--- include/aw/utility/test.h
+++ include/aw/utility/test.h
@@ -227,31 +227,64 @@ class report_junit : public report {
 public:
 	void begin_suite(const char* name, int test_count) override
 	{
-		println("<testsuite tests=\"", test_count, "\" name=\"", name, "\">");
+		test_cases.clear();
+		succeeded = 0;
+		failed    = 0;
+		this->name = name;
+		this->total = test_count;
 	}
 
 	void end_suite()
 	{
+		int skipped = total - succeeded - failed;
+		print("<testsuite name=\"", name, "\" tests=\"", total, "\" errors=\"0\" ");
+		println("failures=\"", failed, "\" skipped=\"", skipped, "\">");
+		for (const auto& test_case : test_cases)
+		{
+			if (test_case.success)
+			{
+				println("<testcase name=\"", test_case.name, "\" time=\"0\"/>");
+			}
+			else
+			{
+				println("<testcase name=\"", test_case.name, "\" time=\"0\">");
+				for (auto& check : test_case.checks)
+				{
+					if (check)
+						continue;
+					println("<failure type=\"check_failed\">" + check.message + "\"</failure>");
+				}
+				println("</testcase>");
+			}
+		}
 		println("</testsuite>");
 
 	}
 
 	void test_success(const char* name, const std::vector<check_report>& checks) override
 	{
-		println("<testcase name=\"", name, "\"/>");
+		++succeeded;
+		test_cases.push_back(test_case{true, name, checks});
 	}
 
 	void test_failure(const char* name, const std::vector<check_report>& checks, const char* detail) override
 	{
-		println("<testcase name=\"", name, "\">");
-		for (auto& check : checks)
-		{
-			if (check)
-				continue;
-			println("<failure type=\"check_failed\">" + check.message + "\"</failure>");
-		}
-		println("</testcase>");
+		++failed;
+		test_cases.push_back(test_case{false, name, checks});
 	}
+
+private:
+	struct test_case {
+		bool success;
+		std::string name;
+		std::vector<check_report> checks;
+	};
+	std::vector<test_case> test_cases;
+
+	std::string name;
+	int total;
+	int succeeded = 0;
+	int failed    = 0;
 };
 
 struct context {
