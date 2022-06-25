aa94ec26c32176f83613ae142b33148a8007f992
Hudd <hededrk@gmail.com>
2022-06-25 09:40:40 +0300

    string: add format header

---
+++ string/include/aw/string/format.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2022       Hudd <hededrk@gmail.com>
+ *
+ * License LGPLv3 or later:
+ * GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
+ * This is free software: you are free to change and redistribute it.
+ * There is NO WARRANTY, to the extent permitted by law.
+ */
+#ifndef aw_string_format_h
+#define aw_string_format_h
+/*
+ * This header wraps std::format and
+ * falls back to fmtlib if it is not available.
+ */
+
+#define AW_NO_FORMAT 0
+#define AW_STD_FORMAT 1
+#define AW_FMTLIB_FORMAT 2
+
+#if __has_include(<format>)
+#include <format>
+#define AW_FORMAT AW_STD_FORMAT
+#elif __has_include(<fmt/format.h>)
+#define AW_FORMAT AW_FMTLIB_FORMAT
+#else
+#define AW_FORMAT AW_NO_FORMAT
+#endif
+
+#if AW_FORMAT == AW_STD_FORMAT
+namespace aw {
+using std::format;
+using std::vformat;
+using std::make_format_args;
+} // namespace aw
+#elif AW_FORMAT == AW_FMTLIB_FORMAT
+#include <fmt/format.h>
+namespace aw {
+using fmt::format;
+using fmt::vformat;
+using fmt::make_format_args;
+}
+#endif
+
+#endif//aw_string_format_h
