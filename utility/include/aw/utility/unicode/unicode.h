44581dfb9e576a7cea72c356085fd3dc1225e3df
Hudd <haddayn@gmail.com>
2023-03-22 04:17:30 +0400

    utility: Refactor unicode codecs
    Various readability imporvements for someone who's unfamiliary with
    unicode.
    Also added an overload that stops upon detecting invalid encoding.

--- utility/include/aw/utility/unicode/unicode.h
+++ utility/include/aw/utility/unicode/unicode.h
@@ -22,17 +22,51 @@ namespace unicode {
 using code_point = std::uint_least32_t;
 constexpr code_point invalid = -1;
 
+enum surrogate : code_point {
+	high_begin = 0xD800,
+	high_end   = 0xDBFF,
+	low_begin  = 0xDC00,
+	low_end    = 0xDFFF,
+	begin      = high_begin,
+	end        = low_end,
+};
+
 //! Check if code point is a surrogate
-inline bool isSurrogate(code_point cp)
+inline bool is_surrogate(code_point cp)
 {
-	return code_point(cp) >= 0xD800 && code_point(cp) <= 0xDFFF;
+	return cp >= surrogate::begin && cp <= surrogate::end;
 }
 
 //! Check if code point is valid
-inline bool isValidCodepoint(code_point cp)
+inline bool is_valid_code_point(code_point cp)
 {
-	return code_point(cp) < 0x110000 && !isSurrogate(cp);
+	return code_point(cp) < 0x110000 && !is_surrogate(cp);
 }
+
+//! Helper class for converting between code points and code units
+template<size_t N, typename Unit>
+class bitword {
+public:
+	enum : size_t {
+		bits = N,
+		mask = (1u << N) - 1u,
+	};
+
+	/*!
+	 * Get nth sextet from a code point \a cp.
+	 * \param n Number of the word. Starts with zero (lowest bits)
+	 */
+	static Unit get(code_point cp, Unit n)
+	{
+		return (cp >> bits*n) & mask;
+	}
+
+	static code_point add(code_point cp, Unit unit)
+	{
+		return (cp << bits) | (unit & mask);
+	}
+};
+
 } // namespace unicode
 } // namespace aw
 #endif//_aw_utf_
--- utility/include/aw/utility/unicode/utf32.h
+++ utility/include/aw/utility/unicode/utf32.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014-2016  absurdworlds
+ * Copyright (C) 2014-2023  Hudd <haddayn@gmail.com>
  *
  * License LGPLv3 or later:
  * GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
@@ -9,30 +9,33 @@
 #ifndef aw_utility_utf32_h
 #define aw_utility_utf32_h
 #include <aw/utility/unicode/unicode.h>
-namespace aw {
-namespace unicode {
-struct utf32 {
+namespace aw::unicode {
+namespace utf32 {
 using char_type   = char32_t;
 using string      = std::u32string;
 
-template<typename Iterator> static
-inline Iterator encode(code_point cp, Iterator output)
-{
-	*(output++) = cp;
-	return output;
-}
+struct codec {
+	using char_type   = utf32::char_type;
+	using string      = utf32::string;
 
-template<typename Iterator> static
-inline Iterator decode(Iterator input, Iterator end, code_point& cp)
-{
-	if (input != end) {
-		cp = *(input++);
-		return input;
+	template<typename Iterator>
+	static Iterator encode(code_point cp, Iterator output)
+	{
+		*(output++) = cp;
+		return output;
 	}
-	cp = -1;
-	return end;
-}
-}; // namespace utf32
-} // namespace unicode
-} // namespace aw
+
+	template<typename Iterator>
+	static Iterator decode(Iterator input, Iterator end, code_point& cp)
+	{
+		if (input != end) {
+			cp = *(input++);
+			return input;
+		}
+		cp = -1;
+		return end;
+	}
+};
+} // namespace utf32
+} // namespace aw::unicode
 #endif//aw_utility_utf32_h
