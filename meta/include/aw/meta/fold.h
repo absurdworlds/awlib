4f16a5aa4cdde4ecc5d247fefeedc1400e2e6a64
Hudd <haddayn@gmail.com>
2023-04-12 13:39:17 +0400

    meta: add fold<>
    Replacement for boost::mpl::fold

---
+++ meta/include/aw/meta/fold.h
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2023  absurdworlds
+ *
+ * License LGPLv3 or later:
+ * GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
+ * This is free software: you are free to change and redistribute it.
+ * There is NO WARRANTY, to the extent permitted by law.
+ */
+#ifndef aw_meta_fold_h
+#define aw_meta_fold_h
+#include <limits>
+#include <aw/types/types.h>
+#include <aw/meta/list_ops.h>
+#include <aw/meta/conditional.h>
+namespace aw {
+namespace meta {
+
+template<
+	template<typename A, typename B> typename Op,
+	typename Init,
+	typename T,
+	typename...Ts>
+struct fold {
+	using type = typename fold<Op, Op<Init, T>, Ts...>::type;
+};
+
+template<
+	template<typename A, typename B> typename Op,
+	typename Init,
+	typename T>
+struct fold<Op, Init, T> {
+	using type = Op<Init, T>;
+};
+
+template<
+	template<typename A, typename B> typename Op,
+	typename Init,
+	typename T,
+	typename...Ts>
+struct fold_apply {
+	using type = typename fold<Op, typename Op<Init, T>::type, Ts...>::type;
+};
+
+template<
+	template<typename A, typename B> typename Op,
+	typename Init,
+	typename T>
+struct fold_apply<Op, Init, T> {
+	using type = typename Op<Init, T>::type;
+};
+
+} // namespace meta
+
+/*!
+ * For each of Ts, applies the binary Opto the result of the
+ * previous application (or Init for the first application)
+ * and the T.
+ *
+ * For example:
+ *
+ * given
+ *
+ *     template<typename L, typename R>>
+ *     using op_t = typename op<L,R>::type;
+ *
+ * the
+ *
+ *     fold<op_t, None, A, B, C, D>
+ *
+ * is the same as
+ *
+ *     op_t<op_t<op_t<op_t<None, A>, B>, C>, D>
+ *
+ * Note that the op_t<> here is a type alias that returns a concrete type,
+ * not a wrapper with ::type in it. If you don't want to declare a type alias,
+ * use the fold_apply<>.
+ */
+template<
+	// Operation to apply to each pair
+	template<typename A, typename B> typename Op,
+	// Initial value
+	typename Init,
+	// List of types
+	typename...Ts>
+using fold = typename meta::fold<Op, Init, Ts...>::type;
+
+template<
+	// Operation to apply to each pair
+	template<typename A, typename B> typename Op,
+	// Initial value
+	typename Init,
+	// List of types
+	typename...Ts>
+using fold_apply = typename meta::fold<Op, Init, Ts...>::type;
+
+template<
+	template<typename A, typename B> typename Op,
+	typename Init,
+	typename...Ts>
+constexpr inline auto fold_v = fold<Op, Init, Ts...>::value;
+
+} // namespace aw
+#endif//aw_meta_fold_h
---
+++ meta/tests/fold.c++
@@ -0,0 +1,55 @@
+#include "aw/meta/fold.h"
+
+namespace aw {
+
+
+template<typename L, typename T>
+struct append;
+
+template<typename... Ls, typename T>
+struct append<meta::list<Ls...>, T>
+{
+	using type = meta::list<Ls...,T>;
+};
+
+template<typename L, typename T>
+using append_t = typename append<L,T>::type;
+
+static_assert( std::is_same_v<
+	fold<
+		append_t,
+		meta::list<>,
+		int,int,int>,
+	meta::list<int,int,int>
+>);
+
+static_assert( std::is_same_v<
+	fold<
+		append_t,
+		meta::list<float>,
+		int,int,int
+	>,
+	meta::list<float,int,int,int>
+>);
+
+template<typename I, typename T>
+struct count_ints;
+
+template<size_t I, typename T>
+struct count_ints<std::integral_constant<size_t, I>, T>
+{
+	using type = std::integral_constant<size_t, I + std::is_integral_v<T>>;
+};
+
+template<typename I, typename T>
+using count_ints_t = typename count_ints<I,T>::type;
+
+static_assert(
+	fold_v<
+		count_ints_t,
+		std::integral_constant<size_t, 0>,
+		int,int,float,int,int,float,int,float
+	> == 5
+);
+
+} // namespace aw
