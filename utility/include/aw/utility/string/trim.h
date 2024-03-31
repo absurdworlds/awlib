426f46953f10d79b5ddfdc06d8c024addbbda934
Hudd <haddayn@gmail.com>
2024-03-31 23:41:00 +0400

    string: separate trim() and trim_if()
    I renamed all trim() functions that take a predicate to trim_if() for
    consistency and put them in a separate header to make the code easier to
    navigate.

--- utility/include/aw/utility/string/trim.h
+++ utility/include/aw/utility/string/trim.h
@@ -9,116 +9,18 @@
  */
 #ifndef aw_string_trim_h
 #define aw_string_trim_h
-#include <algorithm>
 #include <string>
 #include <aw/types/string_view.h>
 namespace aw {
 namespace string {
-/*!
- * Remove characters matching predicate \a pred
- * from the *beginning* of the string \a str.
- */
-template <typename Predicate>
-std::string& ltrim(std::string& str, Predicate pred)
-{
-	auto first_not = std::find_if_not(begin(str), end(str), pred);
-	str.erase(begin(str), first_not);
-	return str;
-}
 
-/*!
- * Remove characters matching predicate \a pred
- * from the *end* of the string \a str.
- */
-template <typename Predicate>
-std::string& rtrim(std::string& str, Predicate pred)
-{
-	auto last_not = std::find_if_not(rbegin(str), rend(str), pred);
-	str.erase(last_not.base(), end(str));
-	return str;
-}
-
-/*!
- * Remove characters matching predicate \a pred
- * from both end of the string \a str.
- */
-template <typename Predicate>
-std::string& trim(std::string& str, Predicate pred)
-{
-	return ltrim(rtrim(str, pred), pred);
-}
-
-/*!
- * Create a l-trimmed copy of a string \a str
- */
-template <typename Predicate>
-std::string ltrimmed(std::string str, Predicate pred)
-{
-	return ltrim(str, pred);
-}
-
-/*!
- * Create a r-trimmed copy of a string \a str
- */
-template <typename Predicate>
-std::string rtrimmed(std::string str, Predicate pred)
-{
-	return rtrim(str, pred);
-}
-
-/*!
- * Create a trimmed copy of a string \a str
- */
-template <typename Predicate>
-std::string trimmed(std::string str, Predicate pred)
-{
-	auto first = std::find_if_not(begin(str), end(str), pred);
-	auto last = std::find_if_not(rbegin(str), rend(str), pred);
-	return str.substr(first - begin(str), last.base() - first);
-}
-
-/*!
- * Remove characters matching predicate \a pred
- * from the *beginning* of the string \a str.
- */
-template <typename Predicate>
-string_view ltrim_if(string_view str, Predicate pred)
-{
-	auto first_not = std::find_if_not(begin(str), end(str), pred);
-	str.remove_prefix(first_not - begin(str));
-	return str;
-}
-
-/*!
- * Remove characters matching predicate \a pred
- * from the *end* of the string \a str.
- */
-template <typename Predicate>
-string_view rtrim_if(string_view str, Predicate pred)
-{
-	auto last_not = std::find_if_not(rbegin(str), rend(str), pred);
-	str.remove_suffix(last_not - rbegin(str));
-	return str;
-}
-
-/*!
- * Remove characters matching predicate \a pred
- * from both end of the string \a str.
- */
-template <typename Predicate>
-string_view trim_if(string_view str, Predicate pred)
-{
-	auto first = std::find_if_not(begin(str), end(str), pred);
-	auto last = std::find_if_not(rbegin(str), rend(str), pred);
-	return str.substr(first - begin(str), last.base() - first);
-}
-
+constexpr string_view whitespace (" \t\v\f\r", 5);
 
 /*!
  * Remove characters in set \a chars
  * from the *beginning* of the string \a str.
  */
-constexpr inline string_view ltrim(string_view str, string_view chars)
+constexpr string_view ltrim(string_view str, string_view chars = whitespace)
 {
 	auto first_not = str.find_first_not_of( chars );
 	if (first_not == str.npos) return {};
@@ -130,7 +32,7 @@ constexpr inline string_view ltrim(string_view str, string_view chars)
  * Remove characters in set \a chars
  * from the *end* of the string \a str.
  */
-constexpr inline string_view rtrim(string_view str, string_view chars)
+constexpr string_view rtrim(string_view str, string_view chars = whitespace)
 {
 	auto last_not = str.find_last_not_of( chars );
 	if (last_not == str.npos) return {};
@@ -142,7 +44,7 @@ constexpr inline string_view rtrim(string_view str, string_view chars)
  * Remove characters in set \a chars
  * from both end of the string \a str.
  */
-constexpr inline string_view trim(string_view str, string_view chars)
+constexpr string_view trim(string_view str, string_view chars = whitespace)
 {
 	auto first = str.find_first_not_of( chars );
 	auto last  = str.find_last_not_of( chars );
