299e377c91c51166880832164dbaf5ca7b20cd0c
Vibe Nuage Agent <vibe@mistral.ai>
2026-08-10 13:18:34 +0000

    io: add missing input_stream include to endian.h
    Co-authored-by: Hedede <Hedede@users.noreply.github.com>

--- io/include/aw/io/endian.h
+++ io/include/aw/io/endian.h
@@ -5,6 +5,7 @@
  * GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
  * This is free software: you are free to change and redistribute it.
  * There is NO WARRANTY, to the extent permitted by law.
+#include <aw/io/input_stream.h>
  */
 #ifndef aw_io_endian_h
 #define aw_io_endian_h
