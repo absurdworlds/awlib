3a3e46288a0a613b785bf774023bd8c587ef6f67
Hudd <mail@hedede.me>
2026-08-12 15:26:57 +0300

    cmake: add sanitiser support
    There are a few failures that were caught by sanitisers during the
    rounds of bugfixing, so it's worth it to add proper sanitiser support.

---
+++ awlib/cmake/aw-sanitizers.cmake
@@ -0,0 +1,44 @@
+include_guard(GLOBAL)
+
+
+set(AW_SANITIZERS "" CACHE STRING
+	"Sanitizers to build with. Allowed options: address, undefined, thread, memory, leak")
+set(AW_SANITIZERS_KNOWN address undefined thread memory leak)
+option(AW_SANITIZER_FAIL_FAST "Make all sanitizer diagnostics fatal" OFF)
+
+function(_aw_configure_sanitizers)
+	# Accept "address,undefined" as well as "address;undefined"
+	string(REPLACE "," ";" sanitizers "${AW_SANITIZERS}")
+
+	foreach (san IN LISTS sanitizers)
+		if (NOT san IN_LIST AW_SANITIZERS_KNOWN)
+			message(FATAL_ERROR
+				"Unknown sanitizer '${san}'. Allowed options: ${AW_SANITIZERS_KNOWN}")
+		endif()
+	endforeach()
+
+	set(compile_flags)
+	set(link_flags)
+	if (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+		#TODO
+	else()
+		string(REPLACE ";" "," sanitizer_list "${sanitizers}")
+
+		set(compile_flags
+			-fsanitize=${sanitizer_list}
+			-fno-omit-frame-pointer
+			-g)
+
+		set(link_flags -fsanitize=${sanitizer_list})
+
+		if (AW_SANITIZER_FAIL_FAST)
+			    list(APPEND compile_flags -fno-sanitize-recover=all)
+		endif()
+
+	endif()
+
+	add_compile_options(${compile_flags})
+	add_link_options(${link_flags})
+endfunction()
+
+_aw_configure_sanitizers()
