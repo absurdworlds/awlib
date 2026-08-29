b2fa2356b14009ead000bbd8c88222138f27a738
Hedede <dev@hedede.me>
2026-08-29 12:48:28 +0000

    math: fix line3d

--- math/include/aw/math/line3d.h
+++ math/include/aw/math/line3d.h
@@ -1,102 +1,101 @@
 /*
- * Copyright (C) 2014  absurdworlds
+ * Copyright (C) 2014-2026 Hedede <dev@hedede.me>
  *
  * License LGPLv3 or later:
  * GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
  * This is free software: you are free to change and redistribute it.
  * There is NO WARRANTY, to the extent permitted by law.
  */
-#ifndef _aw_Line3d_
-#define _aw_Line3d_
-#include <aw/math/Vector3d.h>
-
-namespace aw {
+#ifndef aw_math_line3d_h
+#define aw_math_line3d_h
+#include <aw/math/vector3d.h>
+namespace aw::math {
 //! Represents a line segment in 3D space
 template<typename T>
-class Segment3d {
-public:
-	//! Default constructor
-	Segment3d()
-		: start(0, 0, 0), end(0, 0, 0)
+struct segment3d {
+	//! Construct a degenerate segment at the origin
+	constexpr segment3d() = default;
+
+	//! Construct a line segment by individual components
+	constexpr segment3d(T ax, T ay, T az, T bx, T by, T bz)
+		: start{ax, ay, az}, end{bx, by, bz}
 	{
 	}
 
-	//! Construct line by individual components
-	Segment3d(T ax, T ay, T az, T bx, T by, T bz)
-		: start(ax, ay, az), end(bx, by, bz)
-	{
-	}
-
-	//! Construct line from start and end vectors
-	Segment3d(Vector3d<T> const& start, Vector3d<T> const& end)
+	//! Construct a line segment from start and end vectors
+	constexpr segment3d(vector3d<T> const& start, vector3d<T> const& end)
 		: start(start), end(end)
 	{
 	}
 
-	//! Assignment operator
-	Segment3d<T>& operator = (Segment3d<T> const& line)
-	{
-		start = line.start;
-		end = line.end;
-
-		return *this;
-	}
-
-	Vector3d<T> getNormalPoint(Vector3d<T> const& point) const
-	{
-		Vector3d<T> a = direction();
-		Vector3d<T> b = point - start;
-
-		a.normalize();
-
-		T projectionLength = b.dot(a);
-
-		return start + a * projectionLength;
-	}
-
-	Vector3d<T> getClosestPoint(Vector3d<T> const& point) const
-	{
-		Vector3d<T> a = vector();
-		Vector3d<T> b = point - start;
-
-		T length = a.length();
-		a /= length;
-
-		T projectionLength = b.dot(a);
-
-		if(projectionLength < 0) {
-			return start;
-		} else if(projectionLength > length) {
-			return end;
-		}
-
-		return start + a * length;
-	}
-
-	Vector3d<T> vector() const
+	//! Vector pointing from \a start to \a end
+	constexpr vector3d<T> to_vector() const
 	{
 		return end - start;
 	}
 
-	//! Get direction vector
-	Vector3d<T> direction() const
+	//! Get unit vector pointing from \a start to \a end
+	vector3d<T> direction() const
 	{
-		return vector().normalized();
+		return normalize(to_vector());
 	}
 
-	Vector3d<T> length() const
+	//! Length of the line segment
+	T length() const
 	{
-		return start.getDistance(end);
+		return distance(start, end);
 	}
 
-public:
-	Vector3d<T> start;
-	Vector3d<T> end;
+	//! Squared length of the segment, avoids computing a square root
+	constexpr T length_sq() const
+	{
+		return distance_sq(start, end);
+	}
+
+	/*!
+	 * Project \a point onto the infinite line passing through
+	 * \a start and \a end.
+	 */
+	vector3d<T> normal_point(vector3d<T> const& point) const
+	{
+		vector3d<T> const dir = direction();
+		vector3d<T> const rel = point - start;
+
+		return start + dir * dot(rel, dir);
+	}
+
+	/*!
+	 * Find the point of the segment which is nearest to \a point.
+	 *
+	 * Unlike normal_point(), the result is clamped to the segment:
+	 * projections falling beyond either end return that end.
+	 */
+	vector3d<T> closest_point(vector3d<T> const& point) const
+	{
+		vector3d<T> const vec = to_vector();
+
+		T const len = vec.length();
+		if (len == T(0))
+			return start;
+
+		vector3d<T> const dir = vec / len;
+
+		T const projection = dot(point - start, dir);
+
+		if (projection < T(0))
+			return start;
+		if (projection > len)
+			return end;
+
+		return start + dir * projection;
+	}
+
+	vector3d<T> start = {};
+	vector3d<T> end   = {};
 };
 
-/*! Alias for Segment3d */
+/*! Alias for segment3d */
 template<typename T>
-using Line3d = Segment3d<T>;
-
-} // namespace aw
-#endif// _aw_Line3d_
+using line3d = segment3d<T>;
+} // namespace aw::math
+#endif//aw_math_line3d_h
