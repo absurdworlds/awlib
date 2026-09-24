5d70e175f99f62519480e3fac319b5df7f515373
Hedede <dev@hedede.me>
2026-09-24 21:07:48 +0000

    functional: cleanup overload
    Removed C++17 fallback and make_overload. It has been deprecated since
    2017, I think 9 years is a long enough grace period.

--- base/include/aw/functional/overload.h
+++ base/include/aw/functional/overload.h
@@ -11,44 +11,18 @@
 #define aw_utility_functional_overload_h
 #include <utility>
 namespace aw {
-#if __cpp_variadic_using >= 201611
 /*!
- * Allows to combine multiple functors into overloaded set.
+ * Allow to build an overload set from multiple lambdas or functors.
  */
 template<typename...Fs>
 struct overload : Fs... {
 	overload() = default;
 	overload(Fs... fs) : Fs{ std::move(fs) }...  {}
 	using Fs::operator()...;
+
 	// Enables use in transparent containers
 	// (std::map, std::unordered_map, etc)
 	using is_transparent = void;
 };
-#else
-template<typename Func, typename...Fs>
-struct overload : overload<Func>, overload<Fs...> {
-	overload(Func func, Fs... fs)
-		: overload<Func>{std::forward<F>(func)},
-		  overload<Fs...>{std::forward<Fs>(fs)...}
-	{}
-
-	using overload<Func>::operator();
-	using overload<Fs...>::operator();
-};
-
-template<typename Func>
-struct overload<Func> : Func {
-	overload(Func func)
-		: Func{std::forward<Func>(func)}
-	{}
-};
-#endif
-
-template<typename...Fs>
-[[deprecated]]
-overload<Fs...> make_overload(Fs&&...fs)
-{
-	return {std::forward<Fs>(fs)...};
-}
 } // namespace aw
 #endif//aw_utility_functional_overload_h
